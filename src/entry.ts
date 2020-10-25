897212a3dbfc3df9a8775d33aef7e07a117fa102
Pier Fumagalli <pier@usrz.com>
2020-10-25 15:35:58 +0100

    Converting indentation to spaces

--- src/entry.ts
+++ src/entry.ts
@@ -4,13 +4,13 @@ import path from 'path'
 /* ========================================================================== */
 
 export function resolve(name: string, directory: string) {
-	const resolved = path.resolve(directory, name)
+  const resolved = path.resolve(directory, name)
   assert(path.isAbsolute(resolved), `Unable to resolve "${name}" in "${directory}"`)
 
-	const relative = path.relative(directory, resolved)
+  const relative = path.relative(directory, resolved)
   assert(! relative.startsWith('..'), `Path "${resolved}" is not in "${directory}"`)
 
-	return { directory, resolved, relative }
+  return { directory, resolved, relative }
 }
 
 /* ========================================================================== *
@@ -19,19 +19,19 @@ export function resolve(name: string, directory: string) {
 
 export enum EntryType {
   FILE = 'FILE',
-  DIRECTORY = 'DIRECTORY', 
+  DIRECTORY = 'DIRECTORY',
 }
 
 export interface AbstractEntry<T extends EntryType> {
   readonly type: T
 
-	readonly from: string
-	readonly source: string
-	readonly fromSourcePath: string
+  readonly from: string
+  readonly source: string
+  readonly fromSourcePath: string
 
-	readonly to?: string
-	readonly target?: string
-	readonly toTargetPath?: string
+  readonly to?: string
+  readonly target?: string
+  readonly toTargetPath?: string
 }
 
 /* ========================================================================== *
@@ -44,26 +44,26 @@ interface BaseEntry<T extends EntryType> extends AbstractEntry<T> {}
 
 /* The `BaseEntry` class implements all methods of `AbstractEntry` */
 class BaseEntry<T extends EntryType> implements AbstractEntry<T> {
-	constructor(type: T, name: string, fromdir: string, todir?: string) {
-		const { directory: from, relative: source, resolved: fromSourcePath } = resolve(name, fromdir)
-		const { directory: to, relative: target, resolved: toTargetPath } = todir ? resolve(name, todir) : {
-			directory: undefined,
-			relative: undefined,
-			resolved: undefined,
-		}
+  constructor(type: T, name: string, fromdir: string, todir?: string) {
+    const { directory: from, relative: source, resolved: fromSourcePath } = resolve(name, fromdir)
+    const { directory: to, relative: target, resolved: toTargetPath } = todir ? resolve(name, todir) : {
+      directory: undefined,
+      relative: undefined,
+      resolved: undefined,
+    }
 
-		Object.defineProperties(this, {
-			type: { enumerable: true, value: type },
+    Object.defineProperties(this, {
+      type: { enumerable: true, value: type },
 
-			from: { enumerable: true, value: from },
-			source: { enumerable: true, value: source },
-			fromSourcePath: { enumerable: true, value: fromSourcePath },
-			
-			to: { enumerable: true, value: to },
-			target: { enumerable: true, value: target },
-			toTargetPath: { enumerable: true, value: toTargetPath },
-		})
-	}
+      from: { enumerable: true, value: from },
+      source: { enumerable: true, value: source },
+      fromSourcePath: { enumerable: true, value: fromSourcePath },
+
+      to: { enumerable: true, value: to },
+      target: { enumerable: true, value: target },
+      toTargetPath: { enumerable: true, value: toTargetPath },
+    })
+  }
 }
 
 /* ========================================================================== *
@@ -71,25 +71,25 @@ class BaseEntry<T extends EntryType> implements AbstractEntry<T> {
  * ========================================================================== */
 
 export class FileEntry extends BaseEntry<EntryType.FILE> {
-	constructor(name: string, from: string, to?: string) {
-		super(EntryType.FILE, name, from, to)
-	}
+  constructor(name: string, from: string, to?: string) {
+    super(EntryType.FILE, name, from, to)
+  }
 }
 
 export class DirectoryEntry extends BaseEntry<EntryType.DIRECTORY> {
-	constructor(name: string, from: string, to?: string) {
-		super(EntryType.DIRECTORY, name, from, to)
-	}
+  constructor(name: string, from: string, to?: string) {
+    super(EntryType.DIRECTORY, name, from, to)
+  }
 }
 
-export type Entry = FileEntry | DirectoryEntry 
+export type Entry = FileEntry | DirectoryEntry
 
 export const Entry = {
-	isFileEntry(entry: AbstractEntry<any>): entry is FileEntry {
-		return entry.type === EntryType.FILE
-	},
+  isFileEntry(entry: AbstractEntry<any>): entry is FileEntry {
+    return entry.type === EntryType.FILE
+  },
 
-	isDirectoryEntry(entry: AbstractEntry<any>): entry is DirectoryEntry {
-		return entry.type === EntryType.DIRECTORY
-	},
+  isDirectoryEntry(entry: AbstractEntry<any>): entry is DirectoryEntry {
+    return entry.type === EntryType.DIRECTORY
+  },
 }
