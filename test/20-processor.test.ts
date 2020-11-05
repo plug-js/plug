98c5138425c7af80033b25ccd9c7b7769e99d6fe
Pier Fumagalli <pier@usrz.com>
2020-11-05 23:58:29 +0100

    Whatever

--- test/20-processor.test.ts
+++ test/20-processor.test.ts
@@ -1,19 +1,28 @@
 import { process } from '../src/processor'
 
-describe('Process', () => {
+import { Plug } from '../src/plug'
+
+describe.skip('Process', () => {
   it('should run', async () => {
-    await process('test/tree/**/*')(<any> null) // todo
+    // await process('test/tree/**/*')(<any> null) // todo
+    const plug = new  Plug()
+
+    function foo() {}
+
+    plug.task(() => { })
+    plug.task(foo)
+
   })
 
-  it('should run to', async () => {
-    await process('test/tree/**/*').to('test/build')(<any> null) // todo
-  })
+  // it('should run to', async () => {
+  //   await process('test/tree/**/*').to('test/build')(<any> null) // todo
+  // })
 
-  it('should run from', async () => {
-    await process('**/*', { from: 'test/tree' })(<any> null) // todo
-  })
+  // it('should run from', async () => {
+  //   await process('**/*', { from: 'test/tree' })(<any> null) // todo
+  // })
 
-  it('should run from and to', async () => {
-    await process('**/*', { from: 'test/tree' }).to('test/build')(<any> null) // todo
-  })
+  // it('should run from and to', async () => {
+  //   await process('**/*', { from: 'test/tree' }).to('test/build')(<any> null) // todo
+  // })
 })
