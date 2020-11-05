cf219e2993571c297b1452032c8a9d37c1534262
Pier Fumagalli <pier@usrz.com>
2020-11-05 23:57:14 +0100

    Export Guard and Assertor

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,9 @@ export type Class<T> = (new (...args: any[]) => T)
 
 /** A _non-empty-array_ of `<T>` elements */
 export type NonEmptyArray<T> = [ T, ... T[] ]
+
+/** A _guard_ checking for the type of an argument */
+export type Guard<T> = (what: any) => what is T
+
+/** A _assertor_ ensuring the type of an argument */
+export type Assertor<T> = (what: any, message?: string) => asserts what is T
