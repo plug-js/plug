a7740aa289a52805c67209149c4d2f75185bb39a
Pier Fumagalli <pier@usrz.com>
2021-06-12 17:52:09 +0200

    Async test

--- test/virtual-file-system.test.ts
+++ test/virtual-file-system.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { VirtualFile, VirtualFileSystem } from '../src/virtual-file-system'
 import { basename } from 'path'
-import { readFileSync } from 'fs'
+import { readFileSync, statSync } from 'fs'
 
 describe('Virtual File System', () => {
   it('should create a new VirtualFileSystem', () => {
@@ -150,17 +150,115 @@ describe('Virtual File System', () => {
     }
   })
 
-  describe('Synchronous Virtual File', () => {
+  describe('Asynchronous Virtual File Access', () => {
+    it('should not access missing or unreadable files', async () => {
+      const file1 = new VirtualFileSystem(__dirname).get('this does not exist')
+      expect(await file1.exists()).to.be.false
+      await expect(file1.contents()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('ENOENT'))
+      await expect(file1.sourceMap()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('ENOENT'))
+      await expect(file1.lastModified()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('ENOENT'))
+
+      const file2 = new VirtualFileSystem(__dirname).get(__dirname)
+      await expect(file2.exists()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('EISDIR'))
+      await expect(file2.contents()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('EISDIR'))
+      await expect(file2.sourceMap()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('EISDIR'))
+      await expect(file2.lastModified()).to.be.rejectedWith(Error)
+          .then((error) => expect(error.code).to.equal('EISDIR'))
+    })
+
+    it('should create a VirtualFile', async () => {
+      function create(contents?: any, sourceMap?: any): VirtualFile {
+        return VirtualFileSystem
+            .builder('/foo')
+            .add('bar.js', contents, sourceMap)
+            .build()
+            .get('bar.js')
+      }
+
+      const file0 = create('')
+      expect(await file0.exists()).to.be.true
+      expect(await file0.contents()).to.equal('')
+      expect(await file0.sourceMap()).to.be.undefined
+      expect(await file0.lastModified()).to.be.closeTo(Date.now(), 2)
+
+      const file1 = create('// foobar')
+      expect(await file1.exists()).to.be.true
+      expect(await file1.contents()).to.equal('// foobar')
+      expect(await file1.sourceMap()).to.be.undefined
+      expect(await file1.lastModified()).to.be.closeTo(Date.now(), 2)
+
+      const data = '//# sourceMappingURL=data:application/json;base64,e30=\n// foo'
+
+      const file2 = create(data) // extract source map (default)
+      expect(await file2.exists()).to.be.true
+      expect(await file2.contents()).to.equal('\n// foo')
+      expect(await file2.sourceMap()).to.eql({})
+      expect(await file2.lastModified()).to.be.closeTo(Date.now(), 2)
+
+      const file3 = create(data, true) // extract source map
+      expect(await file3.exists()).to.be.true
+      expect(await file3.contents()).to.equal('\n// foo')
+      expect(await file3.sourceMap()).to.eql({})
+      expect(await file3.lastModified()).to.be.closeTo(Date.now(), 2)
+
+      const file4 = create(data, false) // do not process source maps
+      expect(await file4.exists()).to.be.true
+      expect(await file4.contents()).to.equal(data)
+      expect(await file4.sourceMap()).to.be.undefined
+      expect(await file4.lastModified()).to.be.closeTo(Date.now(), 2)
+
+      const file5 = create(data, { foo: 'bar' }) // supplied source map
+      expect(await file5.exists()).to.be.true
+      expect(await file5.contents()).to.equal(data)
+      expect(await file5.sourceMap()).to.eql({ foo: 'bar' })
+      expect(await file5.lastModified()).to.be.closeTo(Date.now(), 2)
+    })
+
+    it('should read a VirtualFile from disk', async () => {
+      const fileSystem = new VirtualFileSystem(__dirname)
+      const file = fileSystem.get(__filename)
+      const relative = basename(__filename)
+
+      expect(file.absolutePath).to.equal(__filename)
+      expect(file.relativePath).to.equal(relative)
+
+      const data = readFileSync(__filename, 'utf8')
+      const lastModified = statSync(__filename).mtimeMs
+
+      expect(await file.exists()).to.be.true
+      expect(await file.lastModified()).to.equal(lastModified)
+
+      const contents = await file.contents()
+      const sourceMap = await file.sourceMap()
+
+      expect(contents).to.be.a('string').with.length.lessThan(data.length)
+      expect(contents).to.equal(data.substr(0, contents.length))
+
+      expect(sourceMap).to.be.an('object')
+      expect(sourceMap?.version).to.equal(3)
+      expect(sourceMap?.file).to.equal(relative)
+    })
+  })
+
+  describe('Synchronous Virtual File Access', () => {
     it('should not access missing or unreadable files', () => {
       const file1 = new VirtualFileSystem(__dirname).get('this does not exist')
       expect(file1.existsSync()).to.be.false
       expect(() => file1.contentsSync()).to.throw(Error).with.property('code', 'ENOENT')
       expect(() => file1.sourceMapSync()).to.throw(Error).with.property('code', 'ENOENT')
+      expect(() => file1.lastModifiedSync()).to.throw(Error).with.property('code', 'ENOENT')
 
       const file2 = new VirtualFileSystem(__dirname).get(__dirname)
       expect(() => file2.existsSync()).to.throw(Error).with.property('code', 'EISDIR')
       expect(() => file2.contentsSync()).to.throw(Error).with.property('code', 'EISDIR')
       expect(() => file2.sourceMapSync()).to.throw(Error).with.property('code', 'EISDIR')
+      expect(() => file2.lastModifiedSync()).to.throw(Error).with.property('code', 'EISDIR')
     })
 
     it('should create a VirtualFile', () => {
@@ -176,11 +274,13 @@ describe('Virtual File System', () => {
       expect(file0.existsSync()).to.be.true
       expect(file0.contentsSync()).to.equal('')
       expect(file0.sourceMapSync()).to.be.undefined
+      expect(file0.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
 
       const file1 = create('// foobar')
       expect(file1.existsSync()).to.be.true
       expect(file1.contentsSync()).to.equal('// foobar')
       expect(file1.sourceMapSync()).to.be.undefined
+      expect(file1.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
 
       const data = '//# sourceMappingURL=data:application/json;base64,e30=\n// foo'
 
@@ -188,21 +288,25 @@ describe('Virtual File System', () => {
       expect(file2.existsSync()).to.be.true
       expect(file2.contentsSync()).to.equal('\n// foo')
       expect(file2.sourceMapSync()).to.eql({})
+      expect(file2.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
 
       const file3 = create(data, true) // extract source map
       expect(file3.existsSync()).to.be.true
       expect(file3.contentsSync()).to.equal('\n// foo')
       expect(file3.sourceMapSync()).to.eql({})
+      expect(file3.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
 
       const file4 = create(data, false) // do not process source maps
       expect(file4.existsSync()).to.be.true
       expect(file4.contentsSync()).to.equal(data)
       expect(file4.sourceMapSync()).to.be.undefined
+      expect(file4.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
 
       const file5 = create(data, { foo: 'bar' }) // supplied source map
       expect(file5.existsSync()).to.be.true
       expect(file5.contentsSync()).to.equal(data)
       expect(file5.sourceMapSync()).to.eql({ foo: 'bar' })
+      expect(file5.lastModifiedSync()).to.be.closeTo(Date.now(), 2)
     })
 
     it('should read a VirtualFile from disk', () => {
@@ -214,8 +318,12 @@ describe('Virtual File System', () => {
       expect(file.relativePath).to.equal(relative)
 
       const data = readFileSync(__filename, 'utf8')
+      const lastModified = statSync(__filename).mtimeMs
       const contents = file.contentsSync()
       const sourceMap = file.sourceMapSync()
+
+      expect(file.existsSync()).to.be.true
+      expect(file.lastModifiedSync()).to.equal(lastModified)
 
       expect(contents).to.be.a('string').with.length.lessThan(data.length)
       expect(contents).to.equal(data.substr(0, contents.length))
