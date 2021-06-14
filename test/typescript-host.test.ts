6a2b8f67a514007a404bd0a6355c583b15276673
Pier Fumagalli <pier@usrz.com>
2021-06-14 15:56:16 +0200

    TypeScriopt host will not write anymore

--- test/typescript-host.test.ts
+++ test/typescript-host.test.ts
@@ -7,63 +7,6 @@ import { caseSensitivePaths } from '../src/utils/paths'
 import { VirtualFileSystem } from '../src/virtual-file-system'
 
 describe('TypeScript Host', () => {
-  it('should regenerate the virtual file system on build', () => {
-    let number = 0
-    const fake = {
-      builder: (): any => {
-        const builder = number ++
-        return {
-          // test that builder re-generates every time it's called
-          build: (): any => `builder ${builder} build ${number ++}`,
-        }
-      },
-    }
-
-    const host = new TypeScriptHost(fake as any)
-
-    // test that builder re-generates every time it's called
-    expect(host.buildFileSystem()).to.equal('builder 0 build 1')
-    expect(host.buildFileSystem()).to.equal('builder 2 build 3')
-    expect(host.buildFileSystem()).to.equal('builder 4 build 5')
-  })
-
-  it('correctly handle the "onError(...)" callbacks', () => {
-    const fake = {
-      builder: (): any => {
-        return {
-          // test exceptions and onError(...) on writeFile
-          add: (filename: string, data: string): never => {
-            throw new Error(`filename ${filename} data ${data}`)
-          },
-        }
-      },
-      // test exceptions and onError(...) on getSourceFile
-      get: (f: string): never => {
-        throw new Error(`get ${f}`)
-      },
-    }
-
-    const host = new TypeScriptHost(fake as any)
-
-    // test exceptions on getSourceFile
-    expect(() => host.getSourceFile('foo', ScriptTarget.ES3))
-        .to.throw(Error, 'get foo')
-
-    // test onError(...) on getSourceFile
-    let message1 = undefined as string | undefined
-    host.getSourceFile('bar', ScriptTarget.ES3, (m) => message1 = m)
-    expect(message1).to.equal('get bar')
-
-    // test exceptions on writeFile
-    expect(() => host.writeFile('foo', 'bar', false))
-        .to.throw(Error, 'filename foo data bar')
-
-    // test onError(...) on writeFile
-    let message2 = undefined as string | undefined
-    host.writeFile('baz', 'xyz', false, (m) => message2 = m)
-    expect(message2).to.equal('filename baz data xyz')
-  })
-
   it('should correctly return the basics required by typescript', () => {
     const host = new TypeScriptHost({
       builder: () => {},
@@ -183,5 +126,34 @@ describe('TypeScript Host', () => {
     expect(host.getSourceFile('bar.jsx', ScriptTarget.ES2017)).to.not.equal(jsx)
     expect(host.getSourceFile('bar.json', ScriptTarget.ES2016)).to.not.equal(json)
     expect(host.getSourceFile('bar.txt', ScriptTarget.ES2015)).to.not.equal(txt)
+  })
+
+  it('correctly handle the "onError(...)" callbacks', () => {
+    const fake = {
+      // test exceptions and onError(...) on getSourceFile
+      get: (f: string): never => {
+        throw new Error(`get ${f}`)
+      },
+    }
+
+    const host = new TypeScriptHost(fake as any)
+
+    // test exceptions on getSourceFile
+    expect(() => host.getSourceFile('foo', ScriptTarget.ES3))
+        .to.throw(Error, 'get foo')
+
+    // test onError(...) on getSourceFile
+    let message1 = undefined as string | undefined
+    host.getSourceFile('bar', ScriptTarget.ES3, (m) => message1 = m)
+    expect(message1).to.equal('get bar')
+
+    // test exceptions on writeFile
+    expect(() => host.writeFile('foo', 'bar', false))
+        .to.throw(Error, 'Cowardly refusing to write foo')
+
+    // test onError(...) on writeFile
+    let message2 = undefined as string | undefined
+    host.writeFile('baz', 'xyz', false, (m) => message2 = m)
+    expect(message2).to.equal('Politely refusing to write files')
   })
 })
