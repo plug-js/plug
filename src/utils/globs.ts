0e2bcd27674bb8cc363f6e53ed07b6d6f51dabd3
Pier Fumagalli <pier@usrz.com>
2021-06-15 23:49:56 +0200

    VFS from globs and test

---
+++ src/utils/globs.ts
@@ -0,0 +1,55 @@
+import { Options, stream } from 'fast-glob'
+import { VirtualFileSystem } from '../virtual-file-system'
+import { DirectoryPath } from './paths'
+
+/**
+ * A subset of `fast-glob`'s own `Options`, only exposing those options which
+ * are relevant to us, and forcedly removing our forced defaults
+ */
+export type GlobOptions = Pick<Options, 'baseNameMatch'
+  | 'braceExpansion' | 'caseSensitiveMatch' | 'concurrency' | 'deep'
+  | 'dot' | 'extglob' | 'followSymbolicLinks' | 'globstar' | 'ignore'
+  | 'throwErrorOnBrokenSymbolicLink'>
+
+const defaults: Options = {
+  absolute: false, // VFS always does the resolution
+  markDirectories: false, // do not mark directories
+  onlyDirectories: false, // no directories
+  onlyFiles: true, // only files
+  stats: false, // no stats
+  suppressErrors: false, // report errors
+  unique: false, // VFS already dedupes files
+}
+
+/**
+ * Create a `VirtualFileSystem` out of a glob match executed in a directory.
+ *
+ * This will also read and cache the contents of the files while streaming the
+ * directory, in order to validate their readability and speed up later stages.
+ */
+export async function glob(
+    directory: DirectoryPath,
+    globs: string[],
+    options: GlobOptions = {},
+): Promise<VirtualFileSystem> {
+  // Prepare the glob options, assigning our defaults and current directory
+  const opts: Options = Object.assign({}, options, defaults, { cwd: directory })
+
+  // As we stream globs, we read their contents. This ensures that files
+  // can be read and processed later on, and optimises TypeScript which uses
+  // only synchronous methods...
+  const promises: Promise<string>[] = []
+
+  // Create our builder, stream our entries, and read the files
+  const builder = VirtualFileSystem.builder(directory)
+  for await (const entry of stream(globs, opts)) {
+    const file = builder.addFile(entry.toString())
+    promises.push(file.contents())
+  }
+
+  // Await for _all_ files to be read (and cached)
+  await Promise.all(promises)
+
+  // Return our filesystem
+  return builder.build()
+}
---
+++ test/globs.test.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai'
+import { existsSync } from 'fs'
+import { dirname, resolve } from 'path'
+import { glob } from '../src/utils/globs'
+import { DirectoryPath } from '../src/utils/paths'
+
+function findDirectory(directory: string): string {
+  if (existsSync(resolve(directory, 'package.json'))) return directory
+  const parent = dirname(directory)
+  if (directory === parent) throw new Error('File "package.json" not found')
+  return findDirectory(parent)
+}
+
+// This directory _may_ be relocated under build, so find it...
+const directory = resolve(findDirectory(__dirname), 'test', 'support') as DirectoryPath
+
+describe('Virtual File System Globs', () => {
+  it('should prepare a virtual file system from some globs', async () => {
+    const fileSystem = await glob(directory, [ '*' ])
+    const files = fileSystem.list().map((file) => file.relativePath)
+    expect(files).to.have.length.greaterThan(1)
+    expect(files).to.include('build.ts')
+  })
+
+  it('should prepare a virtual file system from some globs and options', async () => {
+    const fileSystem = await glob(directory, [ '*' ], { ignore: [ '*.ts' ] })
+    const files = fileSystem.list().map((file) => file.relativePath)
+    expect(files).to.have.length.greaterThan(1)
+    expect(files).not.to.include('build.ts')
+  })
+})
