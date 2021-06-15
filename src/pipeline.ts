c11eadc9ae14aa75cfbfcf9bb0884bf585c94c6d
Pier Fumagalli <pier@usrz.com>
2021-06-16 01:21:14 +0200

    Pipelines and their tests

---
+++ src/pipeline.ts
@@ -0,0 +1,114 @@
+import { Plug, PlugProcessor } from './plug'
+import { getProjectDirectory } from './project'
+import { ConstructorOverloads } from './types/overloads'
+import { glob, GlobOptions } from './utils/globs'
+import { getDirectoryPath } from './utils/paths'
+import { VirtualFileSystem } from './virtual-file-system'
+
+// At least one glob, and optional options at the end
+type ReadArguments = [ string, ...string[], GlobOptions ] | [ string, ...string[] ]
+
+/** A convenience type describing a `Plug` constructor */
+type PlugConstructor<PlugInstance extends Plug> = new (...args: any) => PlugInstance
+
+/**
+ * A convenience type used to annotate plug instances installed ad extension
+ * to the pipeline with install.
+ */
+export type PlugExtension<C extends PlugConstructor<I>, I extends Plug = Plug> =
+    (...args: ConstructorOverloads<C>) => Pipeline
+
+/* ========================================================================== *
+ * CORE PIPELINE IMPLEMENTATION                                               *
+ * ========================================================================== */
+
+export class Pipeline {
+  #plugs: PlugProcessor[] = []
+  #read: () => Promise<VirtualFileSystem>
+
+  protected constructor(read: () => Promise<VirtualFileSystem>) {
+    this.#read = read
+  }
+
+  plug(plug: PlugProcessor): this {
+    this.#plugs.push(plug)
+    return this
+  }
+
+  /* ======================================================================== */
+
+  static async run(pipeline: Pipeline): Promise<VirtualFileSystem> {
+    let fs = await pipeline.#read()
+    for (const plug of pipeline.#plugs) fs = await plug(fs)
+    return fs
+  }
+
+  /* ======================================================================== */
+
+  /**
+   * Contextualize a pipeline's base directory: any `read(...)` operation
+   * performed by such contextualized pipeline will process globs from the
+   * directory specified here.
+   */
+  static from(directory: string): { read: typeof Pipeline.read } {
+    const projectDir = getProjectDirectory()
+    const baseDir = directory ? getDirectoryPath(projectDir, directory) : projectDir
+
+    return { read: (...args: ReadArguments) => new Pipeline(async () => {
+      const last = args.splice(-1)[0]
+      const { globs, options } = typeof last === 'string' ? {
+        globs: [ ...args as string[], last ],
+        options: {},
+      } : {
+        globs: [ ...args as string[] ],
+        options: last,
+      }
+
+      return await glob(baseDir, globs, options)
+    }) }
+  }
+
+  /**
+   * Create a pipeline reading the files identified by the specified globs
+   * in the current project directory.
+   */
+  static read(...args: ReadArguments): Pipeline {
+    return Pipeline.from('').read(...args)
+  }
+
+  /**
+   * Install the specified plug processor as a standard element in our pipelines.
+   *
+   * This function will return a constructor wrapper function that can be used
+   * as an argument to "pipe.plug(...)".
+   */
+  static install<C extends PlugConstructor<I>, I extends Plug>(
+      name: string,
+      constructor: C,
+  ): (...args: ConstructorOverloads<C>) => PlugProcessor {
+    function create<P extends any[] = ConstructorParameters<C>>(...args: P): PlugProcessor {
+      const instance = new constructor(...args)
+      return instance.process.bind(instance)
+    }
+
+    Object.defineProperty(Pipeline.prototype, name, {
+      value: function<P extends any[] = ConstructorParameters<C>>(this: Pipeline, ...args: P) {
+        const created = create(args)
+        return this.plug(created)
+      },
+    })
+
+    return create
+  }
+}
+
+/* ========================================================================== */
+
+// Freeze our "plug" method in the prototype so that noone can overwrite
+// it with "install" from Plug
+Object.defineProperty(Pipeline.prototype, 'plug', {
+  value: Pipeline.prototype.plug,
+  configurable: false,
+  enumerable: false,
+  writable: false,
+})
---
+++ test/pipeline.test.ts
@@ -0,0 +1,84 @@
+import { expect } from 'chai'
+import { relative } from 'path'
+import { Plug } from '../src/plug'
+import { Pipeline } from '../src/pipeline'
+import { getProjectDirectory } from '../src/project'
+
+describe('Plug Pipelines', () => {
+  it('should construct a simple pipeline', async () => {
+    const pipe = Pipeline.from(__dirname).read('**/*')
+    const fileSystem = await Pipeline.run(pipe)
+    expect(fileSystem.directoryPath).to.equal(__dirname)
+
+    const files = fileSystem.list().map((file) => file.absolutePath)
+    expect(files).to.include(__filename)
+  })
+
+  it('should construct a simple pipeline from the project directory', async () => {
+    const file = relative(getProjectDirectory(), __filename)
+    const pipe = Pipeline.read('**/*', { ignore: [ file ], deep: 1 })
+    const fileSystem = await Pipeline.run(pipe)
+    expect(fileSystem.directoryPath).to.equal(getProjectDirectory())
+
+    const files = fileSystem.list().map((file) => file.absolutePath)
+    expect(files).to.have.length.greaterThan(1)
+    expect(files).not.to.include(__filename)
+  })
+
+  it('should construct and run a multi-stage pipeline', async () => {
+    const pipe = new class extends Pipeline {
+      constructor() {
+        super(() => Promise.resolve('0' as any))
+      }
+    }
+
+    const p1 = pipe.plug((input) => input + '...1' as any)
+    expect(p1).to.equal(pipe)
+
+    const p2 = pipe.plug((input) => Promise.resolve(input + '...2' as any))
+    expect(p2).to.equal(pipe)
+
+    expect(await Pipeline.run(pipe)).to.equal('0...1...2')
+  })
+
+  it('should install a plug in the pipeline', async () => {
+    class TestPlug implements Plug {
+      private instance: number
+
+      constructor(instance: number) {
+        this.instance = instance
+      }
+
+      process(input: any): any {
+        return `${input}...[${this.instance}]` as any
+      }
+    }
+
+    const installed = Pipeline.install('test123', TestPlug)
+
+    const processor = installed(0)
+    expect(processor('foo' as any)).to.equal('foo...[0]')
+
+    const pipe = new class extends Pipeline {
+      constructor() {
+        super(() => Promise.resolve('0' as any))
+      }
+    }
+
+    expect((<any> pipe)['test123']).to.be.a('function')
+
+    const p1 = pipe.plug((input) => input + '...1' as any)
+    expect(p1).to.equal(pipe)
+
+    const p2 = pipe.plug(installed(2))
+    expect(p2).to.equal(pipe)
+
+    const p3 = (<any> pipe).test123(3)
+    expect(p3).to.equal(pipe)
+
+    const p4 = pipe.plug((input) => Promise.resolve(input + '...4' as any))
+    expect(p4).to.equal(pipe)
+
+    expect(await Pipeline.run(pipe)).to.equal('0...1...[2]...[3]...4')
+  })
+})
