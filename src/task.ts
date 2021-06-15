f556768b7a44bb63087d93be417c1e21bf00621a
Pier Fumagalli <pier@usrz.com>
2021-06-16 01:49:14 +0200

    Prototype task definition

---
+++ src/task.ts
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import { Pipeline } from './pipeline'
+
+type TaskSource = (() => Pipeline) | Pipeline
+
+export class PlugTask extends Pipeline {
+  #description: string | undefined
+
+  protected constructor(source: TaskSource, description?: string) {
+    super(() => Pipeline.run(typeof source === 'function' ? source() : source))
+    this.#description = description
+  }
+
+  get description(): string | undefined {
+    return this.#description
+  }
+
+  run(): Promise<void> {
+    return Pipeline.run(this).then(() => {})
+  }
+
+  /* ======================================================================== */
+
+  static task(source: TaskSource): PlugTask
+  static task(description: string, source: TaskSource): PlugTask
+
+  static task(descriptionOrSource: string | TaskSource, optionalSource?: TaskSource): PlugTask {
+    const { description, source } = typeof descriptionOrSource === 'string' ?
+      { description: descriptionOrSource, source: optionalSource } :
+      { description: undefined, source: descriptionOrSource }
+    assert(source, 'Task source missing')
+    return new PlugTask(source, description)
+  }
+}
