9c17caa2c6c9c56beaa538faa57256da93d743df
Pier Fumagalli <pier@usrz.com>
2021-06-16 01:49:40 +0200

    Bootstrap and pseudo build file

---
+++ plugfile.ts
@@ -0,0 +1,22 @@
+import { from, task } from './src'
+
+const typescript: any = null
+const eslint: any = null
+const mocha: any = null
+
+const sources = task('Read source code', () =>
+  from('src')
+      .read('**/*.ts'))
+
+export const lint = task('Lint sources', () =>
+  sources
+      .plug(eslint()))
+
+export const compile = task('Compile source code', () =>
+  lint
+      .plug(typescript())
+      .save('build'))
+
+export const test = task('Test compiled code', () =>
+  compile
+      .plug(mocha()))
