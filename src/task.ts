6cc3f05a29ae98223a6de8fbd7f1661442353ebf
Pier Fumagalli <pier@usrz.com>
2021-06-16 20:49:13 +0200

    Task implementation

--- src/task.ts
+++ src/task.ts
@@ -1,18 +1,20 @@
 import assert from 'assert'
+import { Plug } from '.'
 import { VirtualFileList } from './files'
 import { Pipe } from './pipe'
+import { getProjectDirectory } from './project'
 
-type TaskCall = (() => Pipe) & {
-  readonly run: () => Promise<void>
+export type TaskCall = (() => Pipe) & {
+  readonly run: () => Promise<VirtualFileList>
   readonly task: Task
 }
 
-export class Task {
+export class Task implements Plug {
   #description: string | undefined
   #source: () => Pipe
 
   private constructor(source: () => Pipe, description?: string) {
-    this.#description = description
+    this.#description = description || undefined // empty string
     this.#source = source
   }
 
@@ -20,9 +22,8 @@ export class Task {
     return this.#description
   }
 
-  run(): Promise<VirtualFileList> {
-    // return Pipe.run(this.#source())\
-    throw new Error()
+  process(input: VirtualFileList): Promise<VirtualFileList> {
+    return this.#source().process(input)
   }
 
   /* ======================================================================== */
@@ -31,19 +32,20 @@ export class Task {
   static task(description: string, source: () => Pipe): TaskCall
 
   static task(descriptionOrSource: string | (() => Pipe), optionalSource?: () => Pipe): TaskCall {
-    // const { description, source } = typeof descriptionOrSource === 'string' ?
-    //   { description: descriptionOrSource, source: optionalSource } :
-    //   { description: undefined, source: descriptionOrSource }
-    // assert(source, 'Task source missing')
+    const { description, source } = typeof descriptionOrSource === 'string' ?
+        { description: descriptionOrSource, source: optionalSource } :
+        { description: undefined, source: descriptionOrSource }
+    assert(source, 'Task source missing')
 
-    // const task = new Task(source, description)
+    // Create our task, with its source and description
+    const task = new Task(source, description)
 
-    // const call = (): Pipe => new Pipe(() => task.run())
-    // call.run = () => task.run().then(() => void 0)
-    // call.task = task
-
-    // return call
-
-    throw new Error('')
+    // Our task call returns a Pipe initially plugged with the task's process
+    const call = (): Pipe => Pipe.pipe().plug(task.process.bind(task))
+    // The "run()" (convenience) method runs the task from the project directory
+    call.run = () => task.process(new VirtualFileList(getProjectDirectory()))
+    // And we just remember our task, too, before returning
+    call.task = task
+    return call
   }
 }
