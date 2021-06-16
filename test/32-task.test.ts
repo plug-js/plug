3c4651257ab4b9fd270cff476602789ff57f3b31
Pier Fumagalli <pier@usrz.com>
2021-06-17 01:45:41 +0200

    Test propagation of task names

--- test/32-task.test.ts
+++ test/32-task.test.ts
@@ -1,16 +1,19 @@
 import { expect } from 'chai'
 import { VirtualFileList } from '../src/files'
 import { Pipe } from '../src/pipe'
-import { getProjectDirectory } from '../src/project'
+import { getProjectDirectory, setTaskName } from '../src/project'
 import { Task } from '../src/task'
 
 describe('Plug Tasks', () => {
   it('should construct a task', async () => {
     let counter = 0
-    const task = Task.task('test task', () => Pipe.pipe().plug((input) => {
+    const task = Task.task('test task', () => Pipe.pipe().plug((input, taskNames) => {
+      expect(taskNames).eql([ 'testTask' ])
       input.add(`bar${++ counter}.txt`)
       return Promise.resolve(input)
     }))
+
+    setTaskName(task.task, 'testTask')
 
     expect(task).to.be.a('function')
     expect(task.run).to.be.a('function')
@@ -32,20 +35,27 @@ describe('Plug Tasks', () => {
 
   it('should chain multiple tasks', async () => {
     let counter = 0
-    const task1 = Task.task('test task', () => Pipe.pipe().plug((input) => {
+    const task1 = Task.task('test task', () => Pipe.pipe().plug((input, taskNames) => {
+      expect(taskNames).to.eql([ 'task3', 'task2', 'task1' ])
       input.add(`foo${++ counter}.txt`)
       return Promise.resolve(input)
     }))
 
-    const task2 = Task.task('', () => task1().plug((input) => {
+    const task2 = Task.task('', () => task1().plug((input, taskNames) => {
+      expect(taskNames).to.eql([ 'task3', 'task2' ])
       input.add(`bar${++ counter}.txt`)
       return Promise.resolve(input)
     }))
 
-    const task3 = Task.task(() => task2().plug((input) => {
+    const task3 = Task.task(() => task2().plug((input, taskNames) => {
+      expect(taskNames).to.eql([ 'task3' ])
       input.add(`baz${++ counter}.txt`)
       return Promise.resolve(input)
     }))
+
+    setTaskName(task1.task, 'task1')
+    setTaskName(task2.task, 'task2')
+    setTaskName(task3.task, 'task3')
 
     expect(task1.task.description).to.equal('test task')
     expect(task2.task.description).to.be.undefined
@@ -69,35 +79,46 @@ describe('Plug Tasks', () => {
     const files = new VirtualFileList('/abc')
 
     let counter = 0
-    const task1 = Task.task(() => Pipe.pipe().plug(async (input) => {
-      expect(input).to.equal(files)
+    const task1 = Task.task(() => Pipe.pipe().plug(async (input, taskNames) => {
       await slow(20) // finish last!
+
+      expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`foo${++ counter}.txt`)
+      list.add(`foo${++ counter}.txt`, `${taskNames?.join()}`)
       list.add('xxx.txt', 'first')
       return list
     }))
 
-    const task2 = Task.task(() => Pipe.pipe().plug(async (input) => {
-      expect(input).to.equal(files)
+    const task2 = Task.task(() => Pipe.pipe().plug(async (input, taskNames) => {
       await slow(10) // finish in between!
+
+      expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`bar${++ counter}.txt`)
+      list.add(`bar${++ counter}.txt`, `${taskNames?.join()}`)
       list.add('xxx.txt', 'second')
       return list
     }))
 
-    const task3 = Task.task(() => Pipe.pipe().plug((input) => {
-      expect(input).to.equal(files)
+    const task3 = Task.task(() => Pipe.pipe().plug((input, taskNames) => {
       // immediate!
+
+      expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`baz${++ counter}.txt`)
+      list.add(`baz${++ counter}.txt`, `${taskNames?.join()}`)
       list.add('xxx.txt', 'third')
       return Promise.resolve(list)
     }))
 
-    counter = 0
     const taskA = Task.parallel(task1, task2, task3)
+    const taskB = Task.parallel('reversed', task3, task2, task1)
+
+    setTaskName(task1.task, 'task1')
+    setTaskName(task2.task, 'task2')
+    setTaskName(task3.task, 'task3')
+    setTaskName(taskA.task, 'taskA')
+    setTaskName(taskB.task, 'taskB')
+
+    counter = 0
     const resultA = await taskA.run(files)
 
     expect(taskA.task.description).to.be.undefined
@@ -108,11 +129,13 @@ describe('Plug Tasks', () => {
       '/abc/xxx.txt',
     ].sort())
     expect(resultA.get('xxx.txt').contentsSync()).to.equal('third') // task3 is last
+    expect(resultA.get('foo3.txt').contentsSync()).to.equal('taskA,task1')
+    expect(resultA.get('bar2.txt').contentsSync()).to.equal('taskA,task2')
+    expect(resultA.get('baz1.txt').contentsSync()).to.equal('taskA,task3')
 
     // REVERSE THE ORDER!
 
     counter = 0
-    const taskB = Task.parallel('reversed', task3, task2, task1)
     const resultB = await taskB.run(files)
 
     expect(taskB.task.description).to.equal('reversed')
@@ -123,5 +146,8 @@ describe('Plug Tasks', () => {
       '/abc/xxx.txt',
     ].sort())
     expect(resultB.get('xxx.txt').contentsSync()).to.equal('first') // task1 is last
+    expect(resultB.get('foo3.txt').contentsSync()).to.equal('taskB,task1')
+    expect(resultB.get('bar2.txt').contentsSync()).to.equal('taskB,task2')
+    expect(resultB.get('baz1.txt').contentsSync()).to.equal('taskB,task3')
   })
 })
