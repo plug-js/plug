7ec90eb679a1a6607dd187a39f56405f0c4af371
Pier Fumagalli <pier@usrz.com>
2021-06-17 03:27:48 +0200

    Do not use `getCurrentDirectoryPath(...)`

--- src/project.ts
+++ src/project.ts
@@ -1,19 +1,20 @@
 import assert from 'assert'
 import { statSync } from 'fs'
+import { resolve } from 'path'
 import { Task } from './task'
-import { DirectoryPath, getCurrentDirectoryPath } from './utils/paths'
+import { DirectoryPath } from './utils/paths'
 
-let projectDir: DirectoryPath = getCurrentDirectoryPath()
+let projectDir = undefined as DirectoryPath | undefined
 
 export function getProjectDirectory(): DirectoryPath {
-  return projectDir
+  return projectDir || process.cwd() as DirectoryPath
 }
 
 export function setProjectDirectory(path: string): void {
-  const directory = getCurrentDirectoryPath(path)
+  const directory = resolve(process.cwd(), path)
   const stat = statSync(directory)
   assert(stat.isDirectory(), `Not a directory: ${directory}`)
-  projectDir = directory
+  projectDir = directory as DirectoryPath
 }
 
 const taskNames = new Map<Task, string>()
