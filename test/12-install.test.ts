c1876948937ad09259816537b438cb6a04ebda8d
Pier Fumagalli <pier@usrz.com>
2021-06-18 19:59:17 +0200

    New install mechanism

--- test/12-install.test.ts
+++ test/12-install.test.ts
@@ -1,7 +1,5 @@
 import { expect } from 'chai'
-import { Plug } from '../src'
-import { Pipe } from '../src/pipe'
-import { install } from '../src/install'
+import { Plug, PlugPipe, install, TaskPipe } from '../src/pipe'
 
 describe('Plug Extensions', () => {
   it('should install a plug in the pipeline', async () => {
@@ -22,15 +20,19 @@ describe('Plug Extensions', () => {
     const processor = installed(0)
     expect(await processor.process('foo' as any, null as any)).to.equal('foo...[0]')
 
-    const pipe = Pipe.pipe()
+    const pipe1 = new PlugPipe()
 
-    expect((<any> pipe)['test123']).to.be.a('function')
+    expect((<any> pipe1)['test123']).to.be.a('function')
 
-    const p1 = pipe.plug((input) => input + '...1' as any)
+    const p1 = pipe1.plug((input) => input + '...1' as any)
     const p2 = p1.plug(installed(2))
     const p3 = (<any> p2).test123(3)
     const p4 = p3.plug((input: any) => Promise.resolve(input + '...4' as any))
 
     expect(await p4.process('0' as any)).to.equal('0...1...[2]...[3]...4')
+
+    const pipe2 = new TaskPipe()
+
+    expect((<any> pipe2)['test123']).to.be.a('function')
   })
 })
