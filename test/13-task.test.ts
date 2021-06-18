0b815254f5bd3bc2ca13fd60280734161749664a
Pier Fumagalli <pier@usrz.com>
2021-06-18 21:25:29 +0200

    Fix tests after rework

--- test/13-task.test.ts
+++ test/13-task.test.ts
@@ -1,125 +1,130 @@
+import { AssertionError } from 'assert'
 import { expect } from 'chai'
 import { VirtualFileList } from '../src/files'
-import { Pipe } from '../src/pipe'
-import { getProjectDirectory, setTaskName } from '../src/project'
-import { Task } from '../src/task'
+import { PlugPipe, Processor } from '../src/pipe'
+import { getProjectDirectory } from '../src/project'
+import { parallel, task } from '../src/task'
 
 describe('Plug Tasks', () => {
-  it('should construct a task', async () => {
+  function make(process: Processor): PlugPipe {
+    return new PlugPipe(undefined, { process })
+  }
+
+  it('should construct a task and run it once', async () => {
     let counter = 0
-    const task = Task.task('test task', () => Pipe.pipe((input, { taskNames }) => {
-      expect(taskNames).eql([ 'testTask' ])
-      input.add(`bar${++ counter}.txt`)
-      return Promise.resolve(input)
-    }))
 
-    setTaskName(task.task, 'testTask')
+    const pipe = make((input) => {
+      counter ++
+      return input
+    })
 
-    expect(task).to.be.a('function')
-    expect(task.run).to.be.a('function')
-    expect(task.task).to.be.instanceof(Task)
-    expect(task.task.description).to.equal('test task')
+    const task1 = task('test task', pipe)
 
-    const list = new VirtualFileList('/foo')
+    expect(task1).to.be.a('function')
+    expect(task1.task).to.be.an('object')
+    expect(task1.task.run).to.be.a('function')
+    expect(task1.task.description).to.equal('test task')
 
-    const result1 = await task.task.process(list, { taskNames: [] })
-    expect(result1.directoryPath).to.equal('/foo')
-    expect(result1.list()).to.have.length(1)
-    expect(result1.list()[0].absolutePath).to.equal('/foo/bar1.txt')
+    const run = {} as any
 
-    const result2 = await task.run()
-    expect(result2.directoryPath).to.equal(getProjectDirectory())
-    expect(result2.list()).to.have.length(1)
-    expect(result2.list()[0].absolutePath).to.equal(`${getProjectDirectory()}/bar2.txt`)
+    // Initial run
+    await task1.task.run(run)
+    expect(counter).to.equal(1)
+
+    // No run, cached output
+    await task1.task.run(run)
+    expect(counter).to.equal(1)
+
+    // New "run", should run again
+    await task1.task.run({} as any)
+    expect(counter).to.equal(2)
   })
 
   it('should chain multiple tasks', async () => {
-    let counter = 0
-    const task1 = Task.task('test task', () => Pipe.pipe((input, { taskNames }) => {
-      expect(taskNames).to.eql([ 'task3', 'task2', 'task1' ])
-      input.add(`foo${++ counter}.txt`)
-      return Promise.resolve(input)
+    let counter1 = 0
+    const task1 = task('test task', make(() => {
+      counter1 ++
+      return 'task1' as any
     }))
 
-    const task2 = Task.task('', () => task1().plug((input, { taskNames }) => {
-      expect(taskNames).to.eql([ 'task3', 'task2' ])
-      input.add(`bar${++ counter}.txt`)
-      return Promise.resolve(input)
+    let counter2 = 0
+    const task2 = task(task1().plug((input) => {
+      counter2 ++
+      expect(input).to.equal('task1')
+      return 'task2' as any
     }))
 
-    const task3 = Task.task(() => task2().plug((input, { taskNames }) => {
-      expect(taskNames).to.eql([ 'task3' ])
-      input.add(`baz${++ counter}.txt`)
-      return Promise.resolve(input)
-    }))
+    const run = {} as any
 
-    setTaskName(task1.task, 'task1')
-    setTaskName(task2.task, 'task2')
-    setTaskName(task3.task, 'task3')
+    // first run task2, it'll invoke task1, then re-invoke task1
+    const result2 = await task2.task.run(run)
+    const result1 = await task1.task.run(run)
+    expect(counter1).to.equal(1)
+    expect(counter2).to.equal(1)
+    expect(result1).to.equal('task1')
+    expect(result2).to.equal('task2')
 
-    expect(task1.task.description).to.equal('test task')
-    expect(task2.task.description).to.be.undefined
-    expect(task3.task.description).to.be.undefined
+    await task1.task.run(run)
+    await task2.task.run(run)
+    expect(counter1).to.equal(1)
+    expect(counter2).to.equal(1)
 
-    const list = new VirtualFileList('/abc')
+    // new runs (both cases), so counters will update
+    await task2.task.run({} as any)
+    await task1.task.run({} as any)
+    expect(counter1).to.equal(3)
+    expect(counter2).to.equal(2)
+  })
 
-    const result1 = await task3.run(list)
-    expect(result1.directoryPath).to.equal('/abc')
-    expect(result1.list()).to.have.length(3)
-    expect(result1.list().map((f) => f.absolutePath).sort()).to.eql([
-      '/abc/foo1.txt',
-      '/abc/bar2.txt',
-      '/abc/baz3.txt',
-    ].sort())
+  it('should fail a task when constructed with the wrong type', async () => {
+    await expect(task({} as any).task.run({} as any))
+        .to.be.rejectedWith(AssertionError, 'Invalid task source')
   })
 
   it('should create a parallel task', async () => {
     const slow = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms))
 
-    const files = new VirtualFileList('/abc')
 
     let counter = 0
-    const task1 = Task.task(() => Pipe.pipe(async (input, { taskNames }) => {
+
+    const files = new VirtualFileList('/abc')
+    const task0 = task(make(() => files))
+
+
+    const task1 = task(() => task0().plug(make(async (input) => {
       await slow(20) // finish last!
 
       expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`foo${++ counter}.txt`, { contents: `${taskNames?.join()}` })
       list.add('xxx.txt', { contents: 'first' })
+      list.add(`foo${++ counter}.txt`)
       return list
-    }))
+    })))
 
-    const task2 = Task.task(() => Pipe.pipe(async (input, { taskNames }) => {
+    const task2 = task(() => task0().plug(make(async (input) => {
       await slow(10) // finish in between!
 
       expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`bar${++ counter}.txt`, { contents: `${taskNames?.join()}` })
       list.add('xxx.txt', { contents: 'second' })
+      list.add(`bar${++ counter}.txt`)
       return list
-    }))
+    })))
 
-    const task3 = Task.task(() => Pipe.pipe((input, { taskNames }) => {
+    const task3 = task(() => task0().plug(make((input) => {
       // immediate!
 
       expect(input).to.equal(files)
       const list = input.clone()
-      list.add(`baz${++ counter}.txt`, { contents: `${taskNames?.join()}` })
       list.add('xxx.txt', { contents: 'third' })
+      list.add(`baz${++ counter}.txt`)
       return Promise.resolve(list)
-    }))
+    })))
 
-    const taskA = Task.parallel(task1, task2, task3)
-    const taskB = Task.parallel('reversed', task3, task2, task1)
-
-    setTaskName(task1.task, 'task1')
-    setTaskName(task2.task, 'task2')
-    setTaskName(task3.task, 'task3')
-    setTaskName(taskA.task, 'taskA')
-    setTaskName(taskB.task, 'taskB')
 
     counter = 0
-    const resultA = await taskA.run(files)
+    const taskA = parallel(task1.task, task2.task, task3.task)
+    const resultA = await taskA.task.run({} as any)
 
     expect(taskA.task.description).to.be.undefined
     expect(resultA.list().map((f) => f.absolutePath).sort()).to.eql([
@@ -128,15 +133,16 @@ describe('Plug Tasks', () => {
       '/abc/baz1.txt', // fastest
       '/abc/xxx.txt',
     ].sort())
-    expect(resultA.get('xxx.txt').contentsSync()).to.equal('third') // task3 is last
-    expect(resultA.get('foo3.txt').contentsSync()).to.equal('taskA,task1')
-    expect(resultA.get('bar2.txt').contentsSync()).to.equal('taskA,task2')
-    expect(resultA.get('baz1.txt').contentsSync()).to.equal('taskA,task3')
+
+    // Always rooted in project path
+    expect(resultA.directoryPath).to.equal(getProjectDirectory())
+    expect(resultA.get('/abc/xxx.txt').contentsSync()).to.equal('third') // task3 is last
 
     // REVERSE THE ORDER!
 
     counter = 0
-    const resultB = await taskB.run(files)
+    const taskB = parallel('reversed', task3, task2, task1)
+    const resultB = await taskB.task.run({} as any)
 
     expect(taskB.task.description).to.equal('reversed')
     expect(resultB.list().map((f) => f.absolutePath).sort()).to.eql([
@@ -145,9 +151,9 @@ describe('Plug Tasks', () => {
       '/abc/baz1.txt', // fastest
       '/abc/xxx.txt',
     ].sort())
-    expect(resultB.get('xxx.txt').contentsSync()).to.equal('first') // task1 is last
-    expect(resultB.get('foo3.txt').contentsSync()).to.equal('taskB,task1')
-    expect(resultB.get('bar2.txt').contentsSync()).to.equal('taskB,task2')
-    expect(resultB.get('baz1.txt').contentsSync()).to.equal('taskB,task3')
+
+    // Always rooted in project path
+    expect(resultB.directoryPath).to.equal(getProjectDirectory())
+    expect(resultB.get('/abc/xxx.txt').contentsSync()).to.equal('first') // task1 is last
   })
 })
