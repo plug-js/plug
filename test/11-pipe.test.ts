e2aad74b6956d193327b984d07f383136e7c7ad0
Pier Fumagalli <pier@usrz.com>
2021-06-18 21:24:22 +0200

    Test failures

--- test/11-pipe.test.ts
+++ test/11-pipe.test.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai'
 import { VirtualFileList } from '../src/files'
 import { PlugPipe, TaskPipe } from '../src/pipe'
-import { getProjectDirectory } from '../src/project'
 
 describe('Plug Pipes', () => {
   it('should construct a simple pipeline', async () => {
@@ -21,18 +20,8 @@ describe('Plug Pipes', () => {
     expect(await pipeline.process('0' as any, null as any)).to.equal('0...1...2...3...4')
   })
 
-  it('should construct a simple task pipeline', async () => {
-    const pipe = new TaskPipe()
-    const result = await pipe.run({} as any)
-    expect(result.directoryPath).to.equal(getProjectDirectory())
-  })
-
-  it('should construct and run a multi-stage task pipeline', async () => {
-    const pipeline = new TaskPipe()
-        .plug((input) => {
-          expect(input.directoryPath).to.equal(getProjectDirectory())
-          return 0 as any
-        })
+  it('should construct and run a task pipeline', async () => {
+    const pipeline = new TaskPipe({ run: () => 0 as any })
         .plug((input) => input + '...1' as any)
         .plug((input) => Promise.resolve(input + '...2' as any))
         .plug({ process: (input) => input + '...3' as any })
@@ -47,9 +36,10 @@ describe('Plug Pipes', () => {
     }).process(new VirtualFileList(), {} as any)
     await expect(p1).to.be.rejectedWith(Error, 'foo')
 
-    const p2 = new TaskPipe().plug(() => {
-      throw new Error('bar')
-    }).run({} as any)
+    const p2 = new TaskPipe({ run: () => null as any })
+        .plug(() => {
+          throw new Error('bar')
+        }).run({} as any)
     await expect(p2).to.be.rejectedWith(Error, 'bar')
   })
 })
