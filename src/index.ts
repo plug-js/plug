ce86f46bf756ae040a803496e732c6c89b6c69f3
Pier Fumagalli <pier@usrz.com>
2021-06-20 19:46:43 +0200

    Some comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+// Export all our installed plugs
 export * from './plugs'
-
+// Export our task creation functions
 export { task, parallel } from './task'
+// Export our pipe creation functions
 export { from, read, pipe } from './init'
