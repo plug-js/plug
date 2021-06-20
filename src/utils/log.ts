b9a64fd6ff2f5a7ffb3b120ea334873e5f93da46
Pier Fumagalli <pier@usrz.com>
2021-06-20 16:03:23 +0200

    Logging and tests

---
+++ src/utils/log.ts
@@ -0,0 +1,149 @@
+import { EOL } from 'os'
+import { inspect } from 'util'
+import { isatty } from 'tty'
+
+import { STYLE, RGB } from './colors'
+
+import { Run } from '../run'
+import { Plug } from '../pipe'
+import { getTaskName } from '../project'
+
+/* ========================================================================== */
+
+/** Our logging levels */
+export enum LogLevel {
+  DEBUG = 0,
+  BASIC = 1,
+  ALERT = 2,
+  ERROR = 3,
+  QUIET = 4,
+}
+
+/** Options to customize the logger */
+export interface LogOptions {
+  /** The function used to write log entries, defaults to `console.log(...)` */
+  write: ((data: string) => void)
+  /** The _level_ of the messages to log (default: `LogLevel.NORMAL`) */
+  level: LogLevel
+  /** Whether to log the current time or not (default: `true`) */
+  times: boolean
+  /** Whether to colorize the output or not (default: `true` if standard output is a _TTY_) */
+  colors: boolean
+}
+
+/** The main `Logger` */
+export type Logger = ((message: string, ...args: any[]) => void) & {
+  /** Emit a _debug_ message */
+  debug(message: string, ...args: any[]): void
+  /** Emit an _alert_ message */
+  alert(message: string, ...args: any[]): void
+  /** Emit an _error_ message */
+  error(message: string, ...args: any[]): void
+} & Readonly<Omit<LogOptions, 'write'>>
+
+/* ========================================================================== */
+
+/* The default configurations from command line */
+export const options: LogOptions = (() => {
+  // eslint-disable-next-line no-console
+  const write = console.log
+  const colors = isatty((<any> process.stdout).fd)
+  const times = true
+
+  let level = LogLevel.BASIC
+  // istanbul ignore next - processed at strtup...
+  switch (process.env.LOG_LEVEL?.toUpperCase()) {
+    case 'DEBUG': level = LogLevel.DEBUG; break
+    case 'INFO': // alias
+    case 'BASIC': level = LogLevel.BASIC; break
+    case 'WARN': // alias
+    case 'ALERT': level = LogLevel.ALERT; break
+    case 'ERROR': level = LogLevel.ERROR; break
+    case 'SILENT': // alias
+    case 'QUIET': level = LogLevel.QUIET; break
+  }
+
+  return { write, level, times, colors }
+})()
+
+/* Emit a logging message, properly format */
+function emit(level: LogLevel, run: Run, plug: Plug | undefined, message: string, ...args: any[]): void {
+  // First check if we _really_ have to log this message
+  if (level < options.level) return
+
+  // Simplify wrinting colors here
+  const strings: string[] = []
+  const push = (col: STYLE | RGB | null, ...args: string[]): void => {
+    options.colors && col ? strings.push(col, ...args) : strings.push(...args)
+  }
+
+  // Reset our color at the beginning of the line
+  push(STYLE.RESET)
+
+  // The current time, if we have to
+  if (options.times) {
+    const now = new Date()
+    push(RGB['#8a8a8a'], now.getHours().toString().padStart(2, '0'))
+    push(RGB['#585858'], ':')
+    push(RGB['#8a8a8a'], now.getMinutes().toString().padStart(2, '0'))
+    push(RGB['#585858'], ':')
+    push(RGB['#8a8a8a'], now.getSeconds().toString().padStart(2, '0'))
+    push(RGB['#585858'], ':')
+    push(RGB['#8a8a8a'], now.getMilliseconds().toString().padStart(3, '0'))
+    push(null, ' ')
+  }
+
+  // A label unless this is a normal _log_ entry...
+  if (level <= LogLevel.DEBUG) {
+    push(RGB['#00005f'], '['); push(RGB['#5f5fff'], 'DEBUG'); push(RGB['#000087'], '] ')
+  } else if (level >= LogLevel.ERROR) {
+    push(RGB['#5f0000'], '['); push(RGB['#ff0000'], 'ERROR'); push(RGB['#5f0000'], '] ')
+  } else if (level >= LogLevel.ALERT) {
+    push(RGB['#5f5f00'], '['); push(RGB['#ffff00'], 'ALERT'); push(RGB['#5f5f00'], '] ')
+  }
+
+  // Tasks and plug
+  if (run.tasks.length) {
+    for (let i = 0; i < run.tasks.length; i ++) {
+      push(RGB['#005f00'], i ? '|' : '{')
+      push(RGB['#00ff00'], getTaskName(run.tasks[i]))
+    }
+    // w(RGB['#00ff00'], taskName)
+    if (plug?.name) {
+      push(RGB['#005f00'], '|')
+      push(RGB['#00af00'], plug.name)
+    }
+    push(RGB['#005f00'], '} ')
+  } else if (plug?.name) {
+    push(RGB['#005f00'], '{')
+    push(RGB['#00af00'], plug.name)
+    push(RGB['#005f00'], '} ')
+  }
+
+  // Message, the least with "w(...)"
+  push(STYLE.RESET, message)
+
+  // All other arguments
+  for (const arg of args) {
+    push(null, arg instanceof Error ? EOL : ' ')
+    push(null, inspect(arg, { colors: options.colors }))
+  }
+
+  // Close up (always reset, convert to string, write)
+  push(STYLE.RESET)
+  options.write(strings.join(''))
+}
+
+/* ========================================================================== */
+
+/* Create a `Logger` for the given `Run` and (optional) `Plug` */
+export function makeLog(run: Run, plug?: Plug): Logger {
+  return Object.defineProperties(emit.bind(undefined, LogLevel.BASIC, run, plug), {
+    'debug': { value: emit.bind(undefined, LogLevel.DEBUG, run, plug) },
+    'alert': { value: emit.bind(undefined, LogLevel.ALERT, run, plug) },
+    'error': { value: emit.bind(undefined, LogLevel.ERROR, run, plug) },
+    'colors': { value: options.colors },
+    'level': { value: options.level },
+    'times': { value: options.times },
+  }) as Logger
+}
---
+++ test/05-utils-log.test.ts
@@ -0,0 +1,167 @@
+// import { expect } from 'chai'
+// import { isatty } from 'tty'
+import { expect } from 'chai'
+import { Files } from '../src/files'
+import { Plug } from '../src/pipe'
+import { setTaskName } from '../src/project'
+import { Run } from '../src/run'
+import { Task } from '../src/task'
+
+import {
+  Logger,
+  LogLevel,
+  LogOptions,
+  makeLog,
+  options,
+} from '../src/utils/log'
+
+type TestLog = Logger & { logs: string[] }
+
+function makeTestLog(opts: Partial<Omit<LogOptions, 'colors'>>): TestLog {
+  const lines: string[] = []
+
+  Object.assign(options, opts, {
+    write: lines.push.bind(lines),
+    colors: false,
+  })
+
+  const logger = makeLog(new Run()) as TestLog
+  logger.logs = lines
+  return logger
+}
+
+describe('Logger', () => {
+  // Default log options to restore after each test
+  const defaults = Object.assign({}, options)
+
+  // Convenience plugs and tasks for tests
+  const plug1: Plug = { process: (i) => i, name: 'myplug' }
+  const task1: Task = { run: () => new Files('/foo') }
+  const task2: Task = { run: () => new Files('/foo') }
+  setTaskName(task1, 'mytask1')
+  setTaskName(task2, 'mytask2')
+
+  afterEach(() => {
+    Object.assign(options, defaults)
+  })
+
+  it('should log at "debug" level', () => {
+    const log = makeTestLog({ level: LogLevel.DEBUG, times: false })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    expect(log.logs).to.eql([
+      'A simple message',
+      '[DEBUG] A debug message',
+      '[ALERT] An alert message',
+      '[ERROR] An error message',
+    ])
+  })
+
+  it('should log at "normal" level', () => {
+    const log = makeTestLog({ level: LogLevel.BASIC, times: false })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    expect(log.logs).to.eql([
+      'A simple message',
+      '[ALERT] An alert message',
+      '[ERROR] An error message',
+    ])
+  })
+
+  it('should log at "alert" level', () => {
+    const log = makeTestLog({ level: LogLevel.ALERT, times: false })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    expect(log.logs).to.eql([
+      '[ALERT] An alert message',
+      '[ERROR] An error message',
+    ])
+  })
+
+  it('should log at "error" level', () => {
+    const log = makeTestLog({ level: LogLevel.ERROR, times: false })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    expect(log.logs).to.eql([ '[ERROR] An error message' ])
+  })
+
+  it('should not log at "quiet" level', () => {
+    const log = makeTestLog({ level: LogLevel.QUIET, times: false })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    expect(log.logs).to.eql([])
+  })
+
+  it('should log with stack traces', () => {
+    const log = makeTestLog({ level: LogLevel.BASIC, times: false })
+    const err = new Error('This is a test error')
+    log('This is a test message', 'string', 1, true, err)
+    expect(log.logs).to.eql([ 'This is a test message \'string\' 1 true\n' + err.stack ])
+  })
+
+  it('should log with times', () => {
+    const log = makeTestLog({ level: LogLevel.DEBUG, times: true })
+
+    log('A simple message')
+    log.debug('A debug message')
+    log.alert('An alert message')
+    log.error('An error message')
+
+    const logs = log.logs
+    expect(logs).to.have.length(4)
+    expect(logs[0]).to.match(/^\d{2}:\d{2}:\d{2}.\d{3} A simple message$/)
+    expect(logs[1]).to.match(/^\d{2}:\d{2}:\d{2}.\d{3} \[DEBUG\] A debug message$/)
+    expect(logs[2]).to.match(/^\d{2}:\d{2}:\d{2}.\d{3} \[ALERT\] An alert message$/)
+    expect(logs[3]).to.match(/^\d{2}:\d{2}:\d{2}.\d{3} \[ERROR\] An error message$/)
+  })
+
+  it('should log to the console', () => {
+    options.level = LogLevel.DEBUG
+    options.times = true
+
+    function test(level: LogLevel, colors: boolean): void {
+      options.colors = colors
+      let run = new Run()
+      for (const task of [ undefined, task1, task2 ]) {
+        if (task) run = run.for(task)
+        for (const plug of [ undefined, plug1 ]) {
+          const log = makeLog(run, plug)
+          switch (level) {
+            case LogLevel.BASIC: log('A simple message', 1, true, { hello: 'world' }); break
+            case LogLevel.DEBUG: log.debug('A debug message', 1, true, { hello: 'world' }); break
+            case LogLevel.ALERT: log.alert('An alert message', 1, true, { hello: 'world' }); break
+            case LogLevel.ERROR: log.error('An error message', 1, true, { hello: 'world' }); break
+          }
+        }
+      }
+    }
+    test(LogLevel.BASIC, true)
+    test(LogLevel.DEBUG, true)
+    test(LogLevel.ALERT, true)
+    test(LogLevel.ERROR, true)
+    // test(LogLevel.BASIC, false)
+    // test(LogLevel.DEBUG, false)
+    // test(LogLevel.ALERT, false)
+    // test(LogLevel.ERROR, false)
+  })
+})
