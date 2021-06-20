1672a76aee1d34434eaf40615f95a1339588f9a5
Pier Fumagalli <pier@usrz.com>
2021-06-20 18:23:57 +0200

    Cleanup

--- src/project.ts
+++ src/project.ts
@@ -1,23 +1,4 @@
-// import assert from 'assert'
-// import { statSync } from 'fs'
-// import { resolve } from 'path'
 import { Task } from './task'
-// import { DirectoryPath } from './utils/paths'
-
-// let projectDir = undefined as DirectoryPath | undefined
-
-// TODO: projectDir needs to be a property of a Run, and set by "from"
-
-// export function getProjectDirectory(): DirectoryPath {
-//   return projectDir || process.cwd() as DirectoryPath
-// }
-
-// export function setProjectDirectory(path: string): void {
-//   const directory = resolve(process.cwd(), path)
-//   const stat = statSync(directory)
-//   assert(stat.isDirectory(), `Not a directory: ${directory}`)
-//   projectDir = directory as DirectoryPath
-// }
 
 const taskNames = new WeakMap<Task, string>()
 
