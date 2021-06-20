3008b221c06aec677f110e98eb544b619c6ef235
Pier Fumagalli <pier@usrz.com>
2021-06-20 02:09:57 +0200

    Proper relocation of files

--- src/plugs/from-to.ts
+++ src/plugs/from-to.ts
@@ -1,24 +1,27 @@
 import { Files } from '../files'
 import { Plug, install } from '../pipe'
+import { getProjectDirectory } from '../project'
+import { getDirectoryPath } from '../utils/paths'
 
 declare module '../pipe' {
   interface Pipe<P extends Pipe<P>> {
-    from: PlugExtension<P, typeof FromToPlug>
-    to: PlugExtension<P, typeof FromToPlug>
+    to: PlugExtension<P, typeof ToPlug>
   }
 }
 
-export class FromToPlug implements Plug {
+export class ToPlug implements Plug {
   #directory: string
 
   constructor(directory: string) {
     this.#directory = directory
   }
 
-  process(files: Files): Files {
-    return files.clone(this.#directory)
+  process(input: Files): Files {
+    const directory = getDirectoryPath(getProjectDirectory(), this.#directory)
+    const output = new Files(directory)
+    for (const file of input.list()) output.add(file.relativePath, file)
+    return output
   }
 }
 
-export const from = install('from', FromToPlug)
-export const to = install('to', FromToPlug)
+export const to = install('to', ToPlug)
--- src/plugs/index.ts
+++ src/plugs/index.ts
@@ -1,4 +1,3 @@
 export { compile } from './compile'
-export { from, to } from './from-to'
-export { read } from './read'
 export { save } from './save'
+export { to } from './from-to'
