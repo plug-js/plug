21e6906672f00f0e5605a64ae64e42f050a35500
Pier Fumagalli <pier@usrz.com>
2021-06-20 02:11:23 +0200

    Re-enable build

--- plugfile.ts
+++ plugfile.ts
@@ -1,4 +1,4 @@
-import { from, pipe, task } from './src'
+import { from, task } from './src'
 
 const typescript: any = null
 const eslint: any = null
@@ -6,24 +6,24 @@ const mocha: any = null
 
 const sources = from('src').read('**/*.ts')
 
-// export const lint = task('Lint sources', () =>
-//   sources
-//       .plug(eslint))
+export const lint = task('Lint sources', () =>
+  sources
+      .plug(eslint))
 
-// export const compile = task('Compile source code', () => {
-//   const q = lint()
-//       .plug(typescript())
-//       .plug(mocha())
-//       .from('foo')
-//       // .plug(lint())
-//       // .save('build')
-//       // .from('doo'))
-//   return q
-// })
+export const compile = task('Compile source code', () => {
+  const q = lint()
+      .plug(typescript())
+      .plug(mocha())
+      // .from('foo')
+      // .plug(lint())
+      // .save('build')
+      // .from('doo'))
+  return q
+})
 
-// export const test = task('Test compiled code', () =>
-//   compile()
-//       .plug(mocha()))
+export const test = task('Test compiled code', () =>
+  compile()
+      .plug(mocha()))
 
 // export const xxx = task('Foo', () =>
 //   pipe().plug(mocha))
