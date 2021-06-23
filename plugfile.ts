66986620902c1c1c6e287e1206504acb57089c59
Pier Fumagalli <pier@usrz.com>
2021-06-24 00:42:55 +0200

    Start working on self compilation

--- plugfile.ts
+++ plugfile.ts
@@ -1,29 +1,6 @@
-import { from, task } from './src'
+import { read, task } from './build/src'
 
-const typescript: any = null
-const eslint: any = null
-const mocha: any = null
-
-const sources = from('src').read('**/*.ts')
-
-export const lint = task('Lint sources', () =>
-  sources
-      .plug(eslint))
-
-export const compile = task('Compile source code', () => {
-  const q = lint()
-      .plug(typescript())
-      .plug(mocha())
-      // .from('foo')
-      // .plug(lint())
-      // .save('build')
-      // .from('doo'))
-  return q
-})
-
-export const test = task('Test compiled code', () =>
-  compile()
-      .plug(mocha()))
-
-// export const xxx = task('Foo', () =>
-//   pipe().plug(mocha))
+export const compile = task('Compile source code', () =>
+  read('src/**/*.ts', 'test/**/*.ts', '!test/support/**.*')
+      .compile('tsconfig.json')
+      .save('bootstrap'))
---
+++ test.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/* eslint-disable strict */
+
+process.env.LOG_LEVEL = 'trace'
+
+const { load } = require('./build/src/project')
+load(process.cwd() + '/plugfile.ts')
+    .then((project) => {
+      console.log(project, project.getTaskNames())
+      return project.runTask('compile')
+    })
+    .catch((error) => {
+      if (typeof error.report === 'function') {
+        console.log(error.report(true))
+      }
+      console.log(error)
+      process.exitCode = 1
+    })
