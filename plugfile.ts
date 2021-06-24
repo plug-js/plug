2668d169c364fca87fe5eb3394e12c99a266e657
Pier Fumagalli <pier@usrz.com>
2021-06-24 18:49:12 +0200

    External source maps (easier on the eyes while debugging)

--- plugfile.ts
+++ plugfile.ts
@@ -3,4 +3,4 @@ import { read, task } from './build/src'
 export const compile = task('Compile source code', () =>
   read('src/**/*.ts', 'test/**/*.ts', '!test/support/**.*')
       .compile('tsconfig.json')
-      .save('bootstrap'))
+      .save('bootstrap', { sourceMaps: 'external' }))
