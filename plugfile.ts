87a24e343cb5b92ccfb9a2f243d55b08647af681
Pier Fumagalli <pier@usrz.com>
2021-06-26 02:47:07 +0200

    Run mocha

--- plugfile.ts
+++ plugfile.ts
@@ -3,4 +3,5 @@ import { read, task } from './bootstrap/src'
 export const compile = task('Compile source code', () =>
   read('src/**/*.ts', 'test/**/*.ts', '!test/support/**.*')
       .compile('tsconfig.json')
+      .mocha('test/**/*.test.ts', '!**/*.d.ts')
       .write('build', { sourceMaps: 'external' }))
