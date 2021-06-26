51cfca45c5e629d801b6707ee39b7d4ba9d690ec
Pier Fumagalli <pier@usrz.com>
2021-06-27 00:09:12 +0200

    Filter on matcher

--- src/plugs/filter.ts
+++ src/plugs/filter.ts
@@ -1,80 +1,50 @@
-// import type { File } from '../files'
-// import type { FilterOptions } from '../types/globs'
-// import type { Log } from '../utils/log'
-// import type { Matcher } from 'picomatch'
-// import type { Plug } from '../pipe'
-// import type { Run } from '../run'
+import type { File } from '../files'
+import type { Log } from '../utils/log'
+import { parseOptions, ParseOptions } from '../utils/options'
+import type { Plug } from '../pipe'
+import type { Run } from '../run'
 
-// import picomatch from 'picomatch'
+import { Files } from '../files'
+import { install } from '../pipe'
+import { getRelativePath } from '../utils/paths'
+import { match, MatchOptions } from '../utils/match'
 
-// import { FILTER_OPTIONS_DEFAULTS } from '../types/globs'
-// import { Files } from '../files'
-// import { install } from '../pipe'
-// // import { parseGlobOptions, GlobParameters } from '../utils/globs'
-// import { getRelativePath } from '../utils/paths'
+declare module '../pipe' {
+  interface Pipe<P extends Pipe<P>> {
+    filter: PlugExtension<P, typeof FilterPlug>
+  }
+}
 
-// declare module '../pipe' {
-//   interface Pipe<P extends Pipe<P>> {
-//     filter: PlugExtension<P, typeof FilterPlug>
-//   }
-// }
+export class FilterPlug implements Plug {
+  #matcher: (path: string) => boolean
 
-// export class FilterPlug<Options extends FilterOptions = FilterOptions> implements Plug {
-//   #matcher: Matcher
-//   #options?: Options
-//   #globs: string[]
+  constructor(...args: ParseOptions<MatchOptions>) {
+    this.#matcher = match(parseOptions(args))
+  }
 
-//   constructor(...args: GlobParameters<Options>) {
-//     const { globs, options } = parseGlobOptions(args)
+  protected filter(input: Files, matchOriginalPaths?: boolean): File[] {
+    const files: File[] = []
+    for (const file of input) {
+      const relative = matchOriginalPaths ?
+          getRelativePath(input.directory, file.originalPath) :
+          file.relativePath
+      if (this.#matcher(relative)) files.push(file)
+    }
+    return files
+  }
 
-//     this.#globs = globs
-//     this.#options = options
+  process(input: Files, run: Run, log: Log): Files | Promise<Files> {
+    if (! input.length) return input
 
-//     const filterOptions = {
-//       ...FILTER_OPTIONS_DEFAULTS,
-//       ...options,
-//     }
+    const files = this.filter(input)
+    const output = new Files(input)
+    files.forEach((file) => output.add(file))
 
-//     const matchOptions = {
-//       basename: filterOptions.baseNameMatch,
-//       nobrace: !filterOptions.braceExpansion,
-//       nocase: !filterOptions.caseSensitiveMatch,
-//       dot: filterOptions.dot,
-//       noextglob: !filterOptions.extglob,
-//       noglobstar: !filterOptions.globstar,
-//       ignore: filterOptions.ignore,
-//     }
+    log.debug('Forwarding', output.length, 'files out of', input.length,
+        '(total of', input.length - output.length, 'files removed)')
 
-//     this.#matcher = picomatch(globs, matchOptions)
-//   }
+    return output
+  }
+}
 
-//   protected get options(): Options {
-//     return this.#options!
-//   }
-
-//   protected filter(input: Files, matchOriginalPaths?: boolean): File[] {
-//     const files: File[] = []
-//     for (const file of input) {
-//       const relative = matchOriginalPaths ?
-//           getRelativePath(input.directory, file.originalPath) :
-//           file.relativePath
-//       if (this.#matcher(relative)) files.push(file)
-//     }
-//     return files
-//   }
-
-//   process(input: Files, run: Run, log: Log): Files | Promise<Files> {
-//     if (! input.length) return input
-
-//     const files = this.filter(input)
-//     const output = new Files(input)
-//     files.forEach((file) => output.add(file))
-
-//     log.debug('Forwarding', output.length, 'files out of', input.length,
-//         '(total of', input.length - output.length, 'files removed)')
-
-//     return output
-//   }
-// }
-
-// export const filter = install('filter', FilterPlug)
+export const filter = install('filter', FilterPlug)
--- test/43-plug-filter.test.ts
+++ test/43-plug-filter.test.ts
@@ -1,79 +1,79 @@
-// import { expect } from 'chai'
-// import { FilePath } from '../src/utils/paths'
-// import { File, Files } from '../src/files'
-// import { PlugPipe } from '../src/pipe'
-// import { FilterPlug } from '../src/plugs/filter'
-// import { mock } from './support'
+import { expect } from 'chai'
+import { FilePath } from '../src/utils/paths'
+import { File, Files } from '../src/files'
+import { PlugPipe } from '../src/pipe'
+import { FilterPlug } from '../src/plugs/filter'
+import { mock } from './support'
 
-// describe('Plug Filter Processor', () => {
-//   it('should be installed', () => {
-//     expect(new PlugPipe().filter).to.be.a('function')
-//   })
+describe('Plug Filter Processor', () => {
+  it('should be installed', () => {
+    expect(new PlugPipe().filter).to.be.a('function')
+  })
 
-//   it('should filter some files', async () => {
-//     const { files, run, log } = mock('/foo')
+  it('should filter some files', async () => {
+    const { files, run, log } = mock('/foo')
 
-//     const file1 = files.add('foo.ts', 'contents')
-//     /* file2   */ files.add('Bar.Ts', 'contents')
-//     /* file3   */ files.add('baz.txt', 'contents')
+    const file1 = files.add('foo.ts', 'contents')
+    /* file2   */ files.add('Bar.Ts', 'contents')
+    /* file3   */ files.add('baz.txt', 'contents')
 
-//     const filter = new FilterPlug('**/*.ts')
-//     const output = await filter.process(files, run, log)
-//     expect(filter).to.have.property('options').undefined
+    const filter = new FilterPlug('**/*.ts')
+    const output = await filter.process(files, run, log)
+    expect(filter).to.have.property('options').undefined
 
-//     expect(output.list()).to.eql([ file1 ])
-//   })
+    expect(output.list()).to.eql([ file1 ])
+  })
 
-//   it('should filter some files with options', async () => {
-//     const { files, run, log } = mock('/foo')
+  it('should filter some files with options', async () => {
+    const { files, run, log } = mock('/foo')
 
-//     const file1 = files.add('foo.ts', 'contents')
-//     const file2 = files.add('Bar.Ts', 'contents')
-//     /* file3   */ files.add('baz.txt', 'contents')
+    const file1 = files.add('foo.ts', 'contents')
+    const file2 = files.add('Bar.Ts', 'contents')
+    /* file3   */ files.add('baz.txt', 'contents')
 
-//     const options1 = { caseSensitiveMatch: true }
-//     const filter1 = new FilterPlug('**/*.ts', options1)
-//     expect(filter1).to.have.property('options').equal(options1)
+    const options1 = { nocase: true }
+    const filter1 = new FilterPlug('**/*.ts', options1)
+    expect(filter1).to.have.property('options').equal(options1)
 
-//     const output1 = await filter1.process(files, run, log)
+    const output1 = await filter1.process(files, run, log)
 
-//     expect(output1.list()).to.eql([ file1 ])
+    expect(output1.list()).to.eql([ file1 ])
 
-//     const options2 = { caseSensitiveMatch: false }
-//     const filter2 = new FilterPlug('**/*.ts', options2)
-//     expect(filter2).to.have.property('options').equal(options2)
+    const options2 = { nocase: false }
+    const filter2 = new FilterPlug('**/*.ts', options2)
+    expect(filter2).to.have.property('options').equal(options2)
 
-//     const output2 = await filter2.process(files, run, log)
+    const output2 = await filter2.process(files, run, log)
 
-//     expect(output2.list().sort()).to.eql([ file2, file1 ])
-//   })
+    expect(output2.list().sort()).to.eql([ file2, file1 ])
+  })
 
-//   it('should filter some files matching original paths', () => {
-//     const { files } = mock('/foo')
+  it('should filter some files matching original paths', () => {
+    const { files } = mock('/foo')
 
-//     const file1 = files.add('foo.js', { contents: 'contents', originalPath: '/foo/foo.ts' as FilePath })
-//     /* file2   */ files.add('bar.js', { contents: 'contents' })
+    const file1 = files.add('foo.js', { contents: 'contents', originalPath: '/foo/foo.ts' as FilePath })
+    /* file2   */ files.add('bar.js', { contents: 'contents' })
 
-//     const filter = new class extends FilterPlug {
-//       constructor() {
-//         super('**/*.ts')
-//       }
+    const filter = new class extends FilterPlug {
+      constructor() {
+        super('**/*.ts')
+      }
 
-//       filter(input: Files, matchOriginals?: boolean): File[] {
-//         return super.filter(input, matchOriginals)
-//       }
-//     }
+      filter(input: Files, matchOriginals?: boolean): File[] {
+        return super.filter(input, matchOriginals)
+      }
+    }
 
 
-//     expect(filter.filter(files)).to.eql([]) // not matching originals
-//     expect(filter.filter(files, false)).to.eql([]) // not matching originals
-//     expect(filter.filter(files, true)).to.eql([ file1 ]) // match originals
-//   })
+    expect(filter.filter(files)).to.eql([]) // not matching originals
+    expect(filter.filter(files, false)).to.eql([]) // not matching originals
+    expect(filter.filter(files, true)).to.eql([ file1 ]) // match originals
+  })
 
-//   it('should not filter an empty file list', () => {
-//     const { files, run, log } = mock('/foo')
-//     const filter = new FilterPlug('**/*.ts', { caseSensitiveMatch: true })
-//     const output = filter.process(files, run, log)
-//     expect(output).to.equal(files)
-//   })
-// })
+  it('should not filter an empty file list', () => {
+    const { files, run, log } = mock('/foo')
+    const filter = new FilterPlug('**/*.ts')
+    const output = filter.process(files, run, log)
+    expect(output).to.equal(files)
+  })
+})
