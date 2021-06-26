4ed9caf78300212b287cde3b79656b5f380a087e
Pier Fumagalli <pier@usrz.com>
2021-06-26 03:00:57 +0200

    Clean the right directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 export PATH=$(dirname $0)/node_modules/.bin:${PATH}
 
-rm -rf build
+rm -rf bootstrap
 tsc --build bootstrap.json || exit 1
 nyc mocha --file 'bootstrap/test/.setup.js' 'bootstrap/test/**/*.test.js'
 # ./test.js
