a401c066629bd77ddfada08b89ddafff333641ed
Pier Fumagalli <pier@usrz.com>
2021-06-27 00:08:43 +0200

    New options parsing

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,22 +1,28 @@
-export type ParsedOptions<O> = { strings: string[], options?: O }
-export type ParseOptions<O> = string[] | [ ...string[], O ]
+export type ParsedOptions<Options> = { globs: string[], options: Options }
+export type ParsedOptionalOptions<Options> = ParsedOptions<Options | undefined>
+export type ParseOptions<Options> = [ string, ...string[] ] | [ string, ...string[], Options ]
 
-/**
- * Parse an array of at least one string, followed by an optional `Options`
- * argument.
- */
-export function parseOptions<Options>(
-    args: ParseOptions<Options>,
-): ParsedOptions<Options> {
+/** Parse an array of at least one string, followed by an optional `Options` argument. */
+export function parseOptions<Options>(args: ParseOptions<Options>): ParsedOptionalOptions<Options>
+/** Parse an array of at least one string, followed by an optional `Options` argument. */
+export function parseOptions<Options>(args: ParseOptions<Options>, defaults?: undefined): ParsedOptionalOptions<Options>
+/** Parse an array of at least one string, followed by an optional `Options` argument, ensuring some defaults are present. */
+export function parseOptions<Options, Defaults extends Options>(args: ParseOptions<Options>, defaults: Defaults): ParsedOptions<Options & Defaults>
+// overloaded implementation
+export function parseOptions<Options, Defaults extends Options>(args: ParseOptions<Options>, defaults?: Defaults): ParsedOptions<Options | Defaults | undefined> {
   const last = args.splice(-1)[0]
 
-  const { patterns: strings, options } = typeof last === 'string' ? {
-    patterns: [ ...args as string[], last ],
-    options: undefined,
-  } : {
-    patterns: [ ...args as string[] ],
-    options: last,
-  }
+  const { globs, options } =
+    typeof last === 'string' ? {
+      globs: [ ...args as string[], last ],
+      options: defaults,
+    } : defaults ? {
+      globs: [ ...args as string[] ],
+      options: { ...defaults, ...last },
+    } : {
+      globs: [ ...args as string[] ],
+      options: last,
+    }
 
-  return { strings, options }
+  return { globs, options }
 }
