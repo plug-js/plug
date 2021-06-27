4eb5d025bc7385ef332befaf0984e3420a6077cf
Pier Fumagalli <pier@usrz.com>
2021-06-27 14:47:34 +0200

    Re-add "mocha"

--- src/plugs/mocha.ts
+++ src/plugs/mocha.ts
@@ -1,88 +1,107 @@
-// import type { FilterOptions } from '../types/globs'
-// import type { Log } from '../utils/log'
-// import type { MochaOptions as Options } from 'mocha'
-// import type { Plug } from '../pipe'
-// import type { Run } from '../run'
+import type { Log } from '../utils/log'
+import type { MochaOptions as Options } from 'mocha'
+import type { Plug } from '../pipe'
+import type { Run } from '../run'
 
-// import { Files } from '../files'
-// import { install } from '../pipe'
-// import { FilterPlug } from './filter'
+import type { File, Files } from '../files'
+import { install } from '../pipe'
 
-// import Mocha from 'mocha'
-// import { parseGlobOptions } from '../utils/globs'
-// import { Failure } from '../failure'
-// import { SourceMapsPlug } from './sourcemaps'
-// import { FilePath } from '../utils/paths'
-// import { setupLoader } from '../utils/loader'
+import Mocha from 'mocha'
+import { Failure } from '../failure'
+import { SourceMapsPlug } from './sourcemaps'
+import { FilePath, getRelativePath } from '../utils/paths'
+import { setupLoader } from '../utils/loader'
+import { parseOptions } from '../utils/options'
+import { match, Matcher, MatchOptions } from '../utils/match'
+import { extname } from 'path'
 
-// declare module '../pipe' {
-//   interface Pipe<P extends Pipe<P>> {
-//     mocha: PlugExtension<P, typeof MochaPlug>
-//   }
-// }
+declare module '../pipe' {
+  interface Pipe<P extends Pipe<P>> {
+    mocha: PlugExtension<P, typeof MochaPlug>
+  }
+}
 
-// interface MochaOptions extends FilterOptions, Options {
-//   matchOriginalPaths?: boolean,
-//   require?: string | string[],
-//   reporter?: string,
-// }
+interface MochaOptions extends MatchOptions, Options {
+  matchOriginalPaths?: boolean,
+  reporter?: string,
+}
 
-// type MochaArguments = [ string, ...string[], MochaOptions ] | [ string, ...string[] ]
+type MochaArguments = [ string, ...string[], MochaOptions ] | [ string, ...string[] ]
 
-// class NullReporter extends Mocha.reporters.Base {
-//   constructor(runner: Mocha.Runner, options?: Options) {
-//     super(runner, options)
-//   }
-// }
+class NullReporter extends Mocha.reporters.Base {
+  constructor(runner: Mocha.Runner, options?: Options) {
+    super(runner, options)
+  }
+}
 
-// export class MochaPlug extends FilterPlug<MochaOptions> implements Plug {
-//   constructor(...args: MochaArguments) {
-//     const { globs, options = {} } = parseGlobOptions(args)
-//     super(...globs, options)
+export class MochaPlug implements Plug {
+  #matchOriginalPaths: boolean
+  #options: Mocha.MochaOptions
+  #matcher: Matcher
 
-//     if (this.options!.matchOriginalPaths === undefined) {
-//       this.options!.matchOriginalPaths = true
-//     }
-//     if (this.options!.reporter === 'null') {
-//       this.options!.reporter = NullReporter as any
-//     }
-//   }
+  constructor(...args: MochaArguments) {
+    const { globs, options: { matchOriginalPaths, ...options } } =
+        parseOptions(args, {
+          matchOriginalPaths: true,
+        })
 
-//   async process(zxinput: Files, run: Run, log: Log): Promise<Files> {
-//     const time = log.start()
-//     const mocha = new Mocha(this.options)
+    this.#matcher = match({ globs, options })
+    this.#matchOriginalPaths = matchOriginalPaths
+    this.#options = options
 
-//     const input = new Files(zxinput)
-//     for (const file of zxinput) {
-//       if (file.absolutePath.endsWith('.d.ts')) continue
-//       input.add(file)
-//     }
+    // Replace "null" with our "NullReporter"
+    if (this.#options.reporter === 'null') this.#options.reporter = NullReporter
+  }
 
-//     const files = this.filter(input, this.options!.matchOriginalPaths)
-//     if (! files.length) throw new Failure('No test files found')
+  async process(input: Files, run: Run, log: Log): Promise<Files> {
+    const time = log.start()
 
-//     for (const file of files) {
-//       log.trace(`Mocha testing with "${file.absolutePath}"`)
-//       mocha.addFile(file.absolutePath)
-//     }
+    // We can only run ".js" files, so let's start filtering stuff out...
+    const files: File[] = []
+    for (const file of input) {
+      if (extname(file.absolutePath) !== '.js') continue
+      const path = this.#matchOriginalPaths ? file.originalPath : file.absolutePath
+      const relativePath = getRelativePath(input.directory, path)
+      if (this.#matcher(relativePath)) files.push(file)
+    }
 
-//     const loadables = await new SourceMapsPlug({ sourceMaps: 'inline' }).process(input, run, log)
-//     const sources = loadables.list().reduce((map, file) =>
-//       map.set(file.absolutePath, file.contentsSync()), new Map<FilePath, string>())
-//     setupLoader(sources)
+    // Fail if we can't find any test file...
+    if (! files.length) throw new Failure('No test files found')
 
-//     const failures = await new Promise<number>((resolve, reject) => {
-//       try {
-//         mocha.run((failures) => resolve(failures))
-//       } catch (error) {
-//         reject(error)
-//       }
-//     })
+    // Sort our matching test files before sending them off to Mocha
+    files.sort((a, b): number => {
+      return this.#matchOriginalPaths ?
+        a.originalPath.localeCompare(b.originalPath) :
+        a.absolutePath.localeCompare(b.absolutePath)
+    })
 
-//     if (failures) throw new Failure(`Mocha detected ${failures} test ${failures > 1 ? 'failures' : 'failure'}`)
-//     log.debug('Mocha processed', files.length, 'test files in', time)
-//     return input
-//   }
-// }
+    // Create Mocha and add all our test files...
+    const mocha = new Mocha(this.#options)
+    for (const file of files) {
+      log.trace(`Mocha testing with "${file.absolutePath}"`)
+      mocha.addFile(file.absolutePath)
+    }
 
-// export const mocha = install('mocha', MochaPlug)
+    const loadables = await new SourceMapsPlug({ sourceMaps: 'inline' }).process(input, run, log)
+    const sources = loadables.list().reduce((map, file) =>
+      map.set(file.absolutePath, file.contentsSync()), new Map<FilePath, string>())
+    setupLoader(sources)
+
+    await mocha.loadFilesAsync()
+    setupLoader()
+
+    const failures = await new Promise<number>((resolve, reject) => {
+      try {
+        mocha.run((failures) => resolve(failures))
+      } catch (error) {
+        reject(error)
+      }
+    })
+
+    if (failures) throw new Failure(`Mocha detected ${failures} test ${failures > 1 ? 'failures' : 'failure'}`)
+    log.debug('Mocha processed', files.length, 'test files in', time)
+    return input
+  }
+}
+
+export const mocha = install('mocha', MochaPlug)
