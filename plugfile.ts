e8d6fd967e4c01aafe31ec831f973648c9d43294
Pier Fumagalli <pier@usrz.com>
2021-06-27 14:49:03 +0200

    Correct globs

--- plugfile.ts
+++ plugfile.ts
@@ -1,7 +1,7 @@
 import { read, task } from './bootstrap/src'
 
 export const compile = task('Compile source code', () =>
-  read('src/**/*.ts', 'test/**/*.ts', '!test/support/**.*')
+  read('src/**/*.ts', 'test/**/*.ts', 'test/.setup.ts', { ignore: 'test/support' })
       .compile('tsconfig.json')
       .mocha('test/**/*.test.ts', '!**/*.d.ts')
       .write('build', { sourceMaps: 'external' }))
