9f8d6546a1e4bcea393c227936b546d160a13407
Pier Fumagalli <pier@usrz.com>
2021-06-27 14:40:07 +0200

    Default ignored directories

--- src/utils/walk.ts
+++ src/utils/walk.ts
@@ -14,37 +14,39 @@ type WalkGenerator = AsyncGenerator<FilePath, void, void>
 // A generator function walking a `DirectoryPath` and yelding `FilePath`
 // results for each entry it finds up until the given `maxDepth`
 async function* walker(
-    directory: DirectoryPath,
-    matcher: (path: string) => boolean,
+    root: string,
+    relative: string,
+    matchFile: (path: string) => boolean,
+    igoreDir: (path: string) => boolean,
     symlinks: boolean,
     maxdepth: number,
     depth: number,
 ): WalkGenerator {
   // Read the directory, including file types
-  const dirents = await fs.readdir(directory, { withFileTypes: true })
+  const dirents = await fs.readdir(join(root, relative), { withFileTypes: true })
 
   // For each entry we determine the full path
   for (const dirent of dirents) {
-    const path = join(directory, dirent.name)
+    const path = join(relative, dirent.name)
 
     // If the entry is a file and matches, yield it
-    if (dirent.isFile() && matcher(path)) yield path as FilePath
+    if (dirent.isFile() && matchFile(path)) yield join(root, path) as FilePath
 
     // If the entry is a directory within our depth, walk it recursively
-    else if (dirent.isDirectory() && (depth < maxdepth)) {
-      const children = walker(path as DirectoryPath, matcher, symlinks, maxdepth, depth + 1)
+    else if (dirent.isDirectory() && (!igoreDir(path)) && (depth < maxdepth)) {
+      const children = walker(root, path, matchFile, igoreDir, symlinks, maxdepth, depth + 1)
       for await (const child of children) yield child
 
     // If this is a symlink and we're told to check them let's see what we have
     } else if (dirent.isSymbolicLink() && symlinks) {
-      const stat = await fs.stat(path)
+      const stat = await fs.stat(join(root, path))
 
       // If the link is a file and matches, yield it
-      if (stat.isFile() && matcher(path)) yield path as FilePath
+      if (stat.isFile() && matchFile(path)) yield join(root, path) as FilePath
 
       // If the link is a directory within our depth, walk it recursively
-      else if (stat.isDirectory() && (depth < maxdepth)) {
-        const children = walker(path as DirectoryPath, matcher, symlinks, maxdepth, depth + 1)
+      else if (stat.isDirectory() && (!igoreDir(path)) && (depth < maxdepth)) {
+        const children = walker(root, path, matchFile, igoreDir, symlinks, maxdepth, depth + 1)
         for await (const child of children) yield child
       }
     }
@@ -65,7 +67,14 @@ export interface WalkOptions extends MatchOptions {
    *
    * @default Infinity
    */
-   maxDepth?: number,
+  maxDepth?: number,
+
+  /**
+   * Whether to allow walking any `node_modules` directory or not.
+   *
+   * @default false
+   */
+  allowNodeModules?: boolean,
 }
 
 /**
@@ -75,10 +84,29 @@ export interface WalkOptions extends MatchOptions {
 export function walk(directory: DirectoryPath, ...args: ParseOptions<WalkOptions>): WalkGenerator {
   const { globs, options: { followSymlinks, maxDepth, ...options } } =
       parseOptions(args, {
+        allowNodeModules: false,
         followSymlinks: true,
         maxDepth: Infinity,
       })
 
+  // Create a negative matches from the ignores not to walk certain directories
+  const directoryGlobs =
+      typeof options.ignore === 'string' ? [ options.ignore ] :
+      Array.isArray(options.ignore) ? [ ...options.ignore ] : []
 
-  return walker(directory, match({ globs, options }), followSymlinks, maxDepth, 0)
+  // Make sure to also ignore node modules or dot directories if we have to
+  if (! options.allowNodeModules) directoryGlobs.push('**/node_modules')
+  if (! options.dot) directoryGlobs.push('**/.*')
+
+  // Create our negative matcher to ignore directories
+  const ignoreDir = directoryGlobs.length === 0 ? () => false : match({
+    options: { ...options, ignore: [] },
+    globs: directoryGlobs,
+  })
+
+  // Create our positive matcher to match files
+  const matchFile = match({ globs, options })
+
+  // Do the walk!
+  return walker(directory, '', matchFile, ignoreDir, followSymlinks, maxDepth, 0)
 }
--- test/15-utils-walk.test.ts
+++ test/15-utils-walk.test.ts
@@ -4,7 +4,7 @@ import { walk } from '../src/utils/walk'
 import { FilePath } from '../src/utils/paths'
 import { extname } from 'path/posix'
 
-describe('Directory Walking', () => {
+describe.only('Directory Walking', () => {
   it('should walk a directory', async () => {
     const generator = walk(directory, '**/*.*')
 
@@ -57,5 +57,59 @@ describe('Directory Walking', () => {
     }
 
     expect(files).to.have.length.greaterThan(1)
+  })
+
+  it('should ignore any "node_modules" directory if told to do so', async () => {
+    const generator1 = walk(directory, '**/*.*')
+    const results1 = []
+    for await (const file of generator1) results1.push(file)
+    expect(results1).to.have.length.greaterThan(1)
+    expect(results1).not.to.include(`${directory}/node_modules/file.txt`)
+
+    const generator2 = walk(directory, '**/*.*', { allowNodeModules: true })
+    const results2 = []
+    for await (const file of generator2) results2.push(file)
+    expect(results2).to.have.length.greaterThan(1)
+    expect(results2).to.include(`${directory}/node_modules/file.txt`)
+  })
+
+  it('should ignore any ".*" directory if told to do so', async () => {
+    const generator1 = walk(directory, '**/*.*')
+    const results1 = []
+    for await (const file of generator1) results1.push(file)
+    expect(results1).to.have.length.greaterThan(1)
+    expect(results1).not.to.include(`${directory}/.hidden/file.txt`)
+
+    const generator2 = walk(directory, '**/*.*', { dot: true })
+    const results2 = []
+    for await (const file of generator2) results2.push(file)
+    expect(results2).to.have.length.greaterThan(1)
+    expect(results2).to.include(`${directory}/.hidden/file.txt`)
+  })
+
+  it('should ignore any ".*" directory if told to do so', async () => {
+    const generator1 = walk(directory, '**/*.*', { ignore: [ 'dir' ] })
+    const results1 = []
+    for await (const file of generator1) results1.push(file)
+    expect(results1).to.have.length.greaterThan(1)
+    expect(results1).not.to.include(`${directory}/.hidden/file.txt`)
+    expect(results1).not.to.include(`${directory}/node_modules/file.txt`)
+    expect(results1).not.to.include(`${directory}/dir/file.txt`)
+
+    const generator2 = walk(directory, '**/*.*', { ignore: 'dir', dot: true, allowNodeModules: true })
+    const results2 = []
+    for await (const file of generator2) results2.push(file)
+    expect(results2).to.have.length.greaterThan(1)
+    expect(results2).to.include(`${directory}/.hidden/file.txt`)
+    expect(results2).to.include(`${directory}/node_modules/file.txt`)
+    expect(results2).not.to.include(`${directory}/dir/file.txt`)
+
+    const generator3 = walk(directory, '**/*.*', { dot: true, allowNodeModules: true })
+    const results3 = []
+    for await (const file of generator3) results3.push(file)
+    expect(results3).to.have.length.greaterThan(1)
+    expect(results3).to.include(`${directory}/.hidden/file.txt`)
+    expect(results3).to.include(`${directory}/node_modules/file.txt`)
+    expect(results3).to.include(`${directory}/dir/file.txt`)
   })
 })
