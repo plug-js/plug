f25dba201809adfef170db1b551559de90cef36c
Pier Fumagalli <pier@usrz.com>
2021-06-28 22:22:24 +0200

    Do not alter paths

--- src/sourcemaps/append.ts
+++ src/sourcemaps/append.ts
@@ -1,6 +1,5 @@
-import { basename, sep } from 'path'
+import { basename } from 'path'
 import { RawSourceMap } from 'source-map'
-import { createFilePath, FilePath, getParent, getRelativeFilePath } from '../utils/paths'
 import { encodeSourceMap } from './encode'
 
 /**
@@ -10,21 +9,11 @@ import { encodeSourceMap } from './encode'
  */
 export const SOURCE_MAPPING_URL = 'sourceMappingURL'
 
-export function appendSourceMap(file: FilePath, source: string, inputSourceMap: RawSourceMap, inline: true, sourceRoot?: string): [ string ]
-export function appendSourceMap(file: FilePath, source: string, inputSourceMap: RawSourceMap, inline: false, sourceRoot?: string): [ string, string ]
-export function appendSourceMap(file: FilePath, source: string, inputSourceMap: RawSourceMap, inline: boolean, sourceRoot?: string): string[] {
+export function appendSourceMap(file: string, source: string, inputSourceMap: RawSourceMap, inline: true, sourceRoot?: string): [ string ]
+export function appendSourceMap(file: string, source: string, inputSourceMap: RawSourceMap, inline: false, sourceRoot?: string): [ string, string ]
+export function appendSourceMap(file: string, source: string, inputSourceMap: RawSourceMap, inline: boolean, sourceRoot?: string): string[] {
   // First of all clone the source map
   const sourceMap = { ...inputSourceMap }
-
-  // re-relativize all our paths before writing: "sources" is relative to
-  // the _source_ file, and we remap them relative to the _target_ file...
-  const sourceDirectory = getParent(file)
-  sourceMap.sources = sourceMap.sources.map((source) => {
-    const absoluteSource = createFilePath(sourceDirectory, source)
-    const relativeSource = getRelativeFilePath(file, absoluteSource)
-    if (relativeSource.startsWith('..' + sep)) return relativeSource
-    return `.${sep}${relativeSource}`
-  })
 
   // then replace the file and inject any source root
   sourceMap.file = basename(file)
