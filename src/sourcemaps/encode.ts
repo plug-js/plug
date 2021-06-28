fd1dd5da9a13ff4159e00e3094a431bce46b5c0f
Pier Fumagalli <pier@usrz.com>
2021-06-28 22:22:38 +0200

    No ignores

--- src/sourcemaps/encode.ts
+++ src/sourcemaps/encode.ts
@@ -1,21 +1,24 @@
+import { assert } from 'console'
 import type { RawSourceMap } from 'source-map'
 
 /** Encode a `RawSourceMap` into a `string` strictly adhering to the spec */
 export function encodeSourceMap(sourceMap: RawSourceMap): string {
+  assert(sourceMap.version.toString() === '3', `Wrong source map version "${sourceMap.version}"`)
+
   // The sourcemap URL clearly states that "version" must come first, so we take
   // no chances here when encoding our source map... call me paranoid...
   // https://sourcemaps.info/spec.html
-  const array = [ `{"version":${JSON.stringify(sourceMap.version /* istanbul ignore next */ || 3)}` ]
-  // istanbul ignore else - the spec says "name" is optional, but we always have it
+  const array = [ '{"version":3' ]
+  // the spec says "name" is optional, but we always have it
   if (sourceMap.file) array.push(`,"file":${JSON.stringify(sourceMap.file)}`)
   // Spec says "sourceRoot" is optional
   if (sourceMap.sourceRoot) array.push(`,"sourceRoot":${JSON.stringify(sourceMap.sourceRoot)}`)
   // Spec says "sources" must be there
-  array.push(`,"sources":${JSON.stringify(sourceMap.sources /* istanbul ignore next */ || [])}`)
+  array.push(`,"sources":${JSON.stringify(sourceMap.sources || [])}`)
   // Spec says "sourcesContent" is optional
   if (sourceMap.sourcesContent) array.push(`,"sourcesContent":${JSON.stringify(sourceMap.sourcesContent)}`)
   // Spec says "names" must be there
-  array.push(`,"names":${JSON.stringify(sourceMap.names /* istanbul ignore next */ || [])}`)
+  array.push(`,"names":${JSON.stringify(sourceMap.names || [])}`)
   // Spec says "mappings" must be there
   array.push(`,"mappings":${JSON.stringify(sourceMap.mappings || '')}`)
   // Done with the fields in the correct order
