604546467de51326264688be40c700c2f9592d46
Pier Fumagalli <pier@usrz.com>
2021-06-29 21:05:51 +0200

    Simplify sourcemaps plug

--- src/plugs/sourcemaps.ts
+++ src/plugs/sourcemaps.ts
@@ -1,14 +1,11 @@
-import type { File } from '../files'
-import type { FilePath } from '../utils/paths'
 import type { Files } from '../files'
 import type { Log } from '../utils/log'
 import type { Plug } from '../pipe'
 import type { Run } from '../run'
-import type { SourceMapOptions } from '../files/sourcemap'
+import { SourceMapOptions, writeSourceMap, WriteSourceMapOptions } from '../files/sourcemap'
 
 import { install } from '../pipe'
 import { parallelize } from '../utils/parallelize'
-import { appendSourceMap } from '../sourcemaps'
 
 declare module '../pipe' {
   interface Pipe<P extends Pipe<P>> {
@@ -16,7 +13,7 @@ declare module '../pipe' {
   }
 }
 
-export interface SourceMapsOptions {
+export interface SourceMapsOptions extends SourceMapOptions {
   /**
    * How to write source maps, whether they need to be `inline`, saved as an
    * external file (`external`), or stripped entirely (`none`)
@@ -24,80 +21,25 @@ export interface SourceMapsOptions {
    * @default 'inline'
    */
   sourceMaps?: 'inline' | 'external' | 'none'
-
-  /**
-   * The `sourceRoot` to inject in source maps
-   *
-   * @default undefined
-   */
-  sourceRoot?: string
-
-  /**
-   * Whether to combine source maps resulting from multiple transformation
-   * stages or not
-   *
-   * @default true
-   */
-  combineSourceMaps?: boolean
-
-  /**
-   * Whether to attach the original sources the the source maps.
-   *
-   * @default false
-   */
-   attachSources?: boolean
 }
 
 export class SourceMapsPlug implements Plug {
-  protected readonly sourceMaps: 'inline' | 'external' | undefined
+  #options: WriteSourceMapOptions
 
-  #sourceMapOptions: SourceMapOptions
-  #sourceRoot?: string
-
-  constructor(options: SourceMapsOptions = {}) {
-    // Other options with defaults...
-    const {
-      sourceMaps = 'inline',
-      sourceRoot = undefined,
-      combineSourceMaps = true,
-      attachSources = false,
-    } = options
-
-    // Setup what we need
-    this.sourceMaps = sourceMaps === 'none' ? undefined : sourceMaps
-    this.#sourceRoot = sourceRoot
-    this.#sourceMapOptions = { attachSources, combineSourceMaps: combineSourceMaps }
-  }
-
-  protected async processFile(from: File, path: FilePath, files: Files): Promise<File[]> {
-    if (this.sourceMaps) {
-      // produce the _real_ sourcemap, combining, attaching, ...
-      const fileSourceMap = await from.sourceMap()
-      const sourceMap = await fileSourceMap?.produceSourceMap(path, this.#sourceMapOptions)
-
-      if (sourceMap) {
-        if (this.sourceMaps === 'inline') {
-          const [ contents ] = appendSourceMap(path, await from.contents(), sourceMap, true, this.#sourceRoot)
-          return [ files.add(path, { contents, sourceMap, originalFile: from }) ] // TODO: fileSourceMaps should have no "file"
-        } else if (this.sourceMaps === 'external') {
-          const [ contents, mapContents ] = appendSourceMap(path, await from.contents(), sourceMap, false, this.#sourceRoot)
-          return [
-            files.add(path + '.map', { contents: mapContents }),
-            files.add(path, { contents, sourceMap, originalFile: from }),
-          ]
-        }
-      }
-    }
-
-    return [ files.add(path, from) ]
+  constructor(options: WriteSourceMapOptions = {}) {
+    this.#options = options
   }
 
   async process(input: Files, run: Run, log: Log): Promise<Files> {
     const output = input.fork()
     const time = log.start()
 
-    await parallelize(input, (file) =>
-      this.processFile(file, file.absolutePath, output))
+    await parallelize(input, async (file) => {
+      const outputs = await writeSourceMap(file, file.absolutePath, this.#options)
+      for (const [ path, contents ] of outputs) {
+        output.add(path, { contents, sourceMap: false })
+      }
+    })
 
     log.debug('Processed source maps for', input.length, 'files in', time)
     return output
