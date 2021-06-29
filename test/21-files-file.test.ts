e7b8c073b00306218a48478b5144ae63b0215841
Pier Fumagalli <pier@usrz.com>
2021-06-29 21:38:44 +0200

    Fix test

--- test/21-files-file.test.ts
+++ test/21-files-file.test.ts
@@ -5,29 +5,29 @@ import { FilePath } from '../src/utils/paths'
 
 describe('File Basics', () => {
   it('should create a simple file', async () => {
+    class FileImpl extends File {
+      #sourcemap: boolean
+
+      constructor(sourcemap: boolean, path: string, original?: File) {
+        super(path as FilePath, original)
+        this.#sourcemap = sourcemap
+      }
+
+      contents(): Promise<string> {
+        throw new Error()
+      }
+
+      contentsSync(): string {
+        throw new Error()
+      }
+
+      sourceMap(): any {
+        throw new Error()
+      }
+    }
+
     try {
       (<any> globalThis).caseSensitivePaths = false
-
-      class FileImpl extends File {
-        #sourcemap: boolean
-
-        constructor(sourcemap: boolean, path: string, original?: File) {
-          super(path as FilePath, original)
-          this.#sourcemap = sourcemap
-        }
-
-        contents(): Promise<string> {
-          throw new Error()
-        }
-
-        contentsSync(): string {
-          throw new Error()
-        }
-
-        sourceMap(): any {
-          if (this.#sourcemap) return { produceSourceMap: (...args: any[]): any => args }
-        }
-      }
 
       const file1 = new FileImpl(true, '/Foo/Bar.txt')
 
@@ -35,16 +35,25 @@ describe('File Basics', () => {
       expect(file1.canonicalPath).to.equal('/foo/bar.txt')
       expect(file1.originalFile).to.be.undefined
 
-      expect(await file1.rawSourceMap({ baz: 123 } as any))
-          .to.eql([ '/Foo/Bar.txt', { baz: 123 } ])
-
       const file2 = new FileImpl(false, '/Foo/Baz.txt', file1)
 
       expect(file2.absolutePath).to.equal('/Foo/Baz.txt')
       expect(file2.canonicalPath).to.equal('/foo/baz.txt')
       expect(file2.originalFile).to.equal(file1)
 
-      expect(await file2.rawSourceMap({ baz: 123 } as any)).to.be.undefined
+      ;(<any> globalThis).caseSensitivePaths = true
+
+      const file3 = new FileImpl(true, '/Foo/Bar.txt')
+
+      expect(file3.absolutePath).to.equal('/Foo/Bar.txt')
+      expect(file3.canonicalPath).to.equal('/Foo/Bar.txt')
+      expect(file3.originalFile).to.be.undefined
+
+      const file4 = new FileImpl(false, '/Foo/Baz.txt', file3)
+
+      expect(file4.absolutePath).to.equal('/Foo/Baz.txt')
+      expect(file4.canonicalPath).to.equal('/Foo/Baz.txt')
+      expect(file4.originalFile).to.equal(file3)
     } finally {
       delete (<any> globalThis).caseSensitivePaths
     }
