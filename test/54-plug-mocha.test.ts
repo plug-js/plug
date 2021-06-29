93663ae9c7e66840fe7702c32f4918db7469ba2b
Pier Fumagalli <pier@usrz.com>
2021-06-30 01:11:41 +0200

    Mocha tests

--- test/54-plug-mocha.test.ts
+++ test/54-plug-mocha.test.ts
@@ -1,10 +1,125 @@
 import { expect } from 'chai'
+import { RawSourceMap } from 'source-map'
 import { PlugPipe } from '../src/pipe'
-import { MochaPlug } from '../src/plugs/mocha'
+import { MochaOptions, MochaPlug } from '../src/plugs/mocha'
+import { caseSensitivePaths } from '../src/utils/paths'
+import { mock } from './support'
 
 describe('Plug Mocha Processor', () => {
+  const { files: input, run, log } = mock('/foo')
+
+  const testSource = input.add('test/mytest.ts', { contents: 'testOriginal', sourceMap: false })
+  input.add('test/mytest.js', {
+    contents: 'testCompiled',
+    sourceMap: { version: 3 } as RawSourceMap,
+    originalFile: testSource,
+  })
+
+  const codeSource = input.add('src/testable.ts', { contents: 'codeOriginal', sourceMap: false })
+  input.add('src/testable.js', {
+    contents: 'codeCompiled',
+    sourceMap: { version: 3 } as RawSourceMap,
+    originalFile: codeSource,
+  })
+
+  input.add('utils/extra.js', { contents: 'utility', sourceMap: false })
+
+  let mochaArgs: Parameters<MochaPlug['runMocha']> | undefined = undefined
+  let failures = 0
+
+  class TestMochaPlug extends MochaPlug {
+    constructor(glob: string, options: MochaOptions = {}) {
+      super(glob, options)
+    }
+    runMocha(...args: Parameters<MochaPlug['runMocha']>): ReturnType<MochaPlug['runMocha']> {
+      mochaArgs = args
+      return Promise.resolve(failures)
+    }
+  }
+
+  /* ======================================================================== */
+
+  beforeEach(() => {
+    mochaArgs = undefined
+    failures = 0
+  })
+
   it('should be installed', async () => {
     expect(new PlugPipe().mocha).to.be.a('function')
-    new MochaPlug('foo') // remove me
+  })
+
+  it('should process some test files', async () => {
+    const mocha = new TestMochaPlug('test/**/*.ts')
+
+    const output = await mocha.process(input, run, log)
+    expect(output).to.equal(input)
+
+    expect(mochaArgs).to.be.an('array').with.length(1)
+
+    const { files, tests, options } = mochaArgs![0]
+
+    expect(files).to.have.length(3)
+    expect(files).to.have.keys([
+      '/foo/src/testable.js',
+      '/foo/test/mytest.js',
+      '/foo/utils/extra.js',
+    ])
+    expect(files.get('/foo/test/mytest.js' as any))
+        .to.equal('testCompiled\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibXl0ZXN0LmpzIiwic291cmNlcyI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==')
+    expect(files.get('/foo/src/testable.js' as any))
+        .to.equal('codeCompiled\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdGFibGUuanMiLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9')
+    expect(files.get('/foo/utils/extra.js' as any)).to.equal('utility')
+
+    expect(tests).to.have.length(1)
+    expect(tests).to.include('/foo/test/mytest.js')
+
+    expect(options).to.eql({ nocase: !caseSensitivePaths() })
+  })
+
+  it('should process some test files matching absolute paths', async () => {
+    const mocha = new TestMochaPlug('test/**/*.js', { matchOriginalPaths: false })
+
+    const output = await mocha.process(input, run, log)
+    expect(output).to.equal(input)
+
+    expect(mochaArgs).to.be.an('array').with.length(1)
+
+    const { files, tests, options } = mochaArgs![0]
+
+    expect(files).to.have.length(3)
+    expect(files).to.have.keys([
+      '/foo/src/testable.js',
+      '/foo/test/mytest.js',
+      '/foo/utils/extra.js',
+    ])
+    expect(files.get('/foo/test/mytest.js' as any))
+        .to.equal('testCompiled\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibXl0ZXN0LmpzIiwic291cmNlcyI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==')
+    expect(files.get('/foo/src/testable.js' as any))
+        .to.equal('codeCompiled\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdGFibGUuanMiLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9')
+    expect(files.get('/foo/utils/extra.js' as any)).to.equal('utility')
+
+    expect(tests).to.have.length(1)
+    expect(tests).to.include('/foo/test/mytest.js')
+
+    expect(options).to.eql({ nocase: !caseSensitivePaths() })
+  })
+
+  it('should fail when no tests are found', async () => {
+    const mocha = new TestMochaPlug('nowhere/**/*.js')
+
+    await expect(mocha.process(input, run, log))
+        .to.be.rejectedWith('Build failed: No test files found')
+  })
+
+  it('should fail when failures are detected', async () => {
+    const mocha = new TestMochaPlug('test/**/*.ts')
+
+    failures = 12
+    await expect(mocha.process(input, run, log))
+        .to.be.rejectedWith('Mocha detected 12 test failures')
+
+    failures = 1 // singular
+    await expect(mocha.process(input, run, log))
+        .to.be.rejectedWith('Mocha detected 1 test failure')
   })
 })
