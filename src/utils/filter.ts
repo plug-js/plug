9d424b81480f3a51fd1002c30d12c34af3ff9199
Pier Fumagalli <pier@usrz.com>
2021-06-30 16:23:29 +0200

    Filter file iterables

---
+++ src/utils/filter.ts
@@ -0,0 +1,41 @@
+import { extname } from 'path/posix'
+import { File } from '../files'
+import { match, MatchOptions } from './match'
+import { parseOptions, ParseOptions } from './options'
+import { DirectoryPath, getRelativePath } from './paths'
+
+export interface FilterOptions extends MatchOptions {
+  matchOriginalPaths?: boolean,
+  scriptsOnly?: boolean,
+}
+
+export function* filter(
+    directory: DirectoryPath,
+    input: Iterable<File>,
+    ...args: ParseOptions<FilterOptions>
+): Generator<File, void, void> {
+  const { globs, options } = parseOptions(args, {
+    matchOriginalPaths: true,
+    scriptsOnly: false,
+  })
+
+  const { matchOriginalPaths, scriptsOnly } = options
+
+  const matcher = match(...globs, options)
+
+  if (matchOriginalPaths) {
+    for (const file of input) {
+      if (scriptsOnly && (extname(file.absolutePath) !== '.js')) continue
+      if (! file.originalFile) continue
+      const original = file.originalFile.absolutePath
+      const relative = getRelativePath(directory, original)
+      if (matcher(relative)) yield file
+    }
+  } else {
+    for (const file of input) {
+      if (scriptsOnly && (extname(file.absolutePath) !== '.js')) continue
+      const relative = getRelativePath(directory, file.absolutePath)
+      if (matcher(relative)) yield file
+    }
+  }
+}
