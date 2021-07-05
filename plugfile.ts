39acf3316e4be042ef21d394e6de3b3b9cb1e3b1
Pier Fumagalli <pier@usrz.com>
2021-07-06 00:10:17 +0900

    Blah blah... testing

--- plugfile.ts
+++ plugfile.ts
@@ -3,5 +3,12 @@ import { read, task } from './bootstrap/src'
 export const compile = task('Compile source code', () =>
   read('src/**/*.ts', 'test/**/*.ts', 'test/.setup.ts', { ignore: 'test/support' })
       .compile('tsconfig.json')
-      .mocha('test/**/*.test.ts')
+      // .mocha('test/**/*.test.ts')
       .write('build', { sourceMaps: 'external' }))
+
+export const instrument = task('Instrument source code', () =>
+  read('src/**/*.ts', 'test/**/*.ts', 'test/.setup.ts', { ignore: 'test/support' })
+      .compile('tsconfig.json')
+      .instrument('**/*.ts')
+      .mocha('test/**/*.test.ts'))
+// .write('instrumented', { sourceMaps: 'external' })
